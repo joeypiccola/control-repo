08ce7a6a3eee0dd5a9f224ed1e53e7c57619c0fb
joeypiccola <joey.piccola@gmail.com>
2017-09-13 12:06:25 -0600

    also install mgmt console

--- site/profile/manifests/iis_install.pp
+++ site/profile/manifests/iis_install.pp
@@ -5,6 +5,7 @@ class profile::iis_install (
   $iis_features = ['web-server','web-webserver','Web-Common-Http','Web-Static-Content']
   
   iis_feature { $iis_features:
-    ensure => 'present',
+    ensure                   => 'present',
+    include_management_tools => true,
   }
 }
