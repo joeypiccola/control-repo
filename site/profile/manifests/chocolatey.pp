ba4016d2c89de89eff15002648965c9541b5da2f
joeypiccola <joey.piccola@gmail.com>
2017-09-12 21:38:53 -0600

    added commas

--- site/profile/manifests/chocolatey.pp
+++ site/profile/manifests/chocolatey.pp
@@ -5,13 +5,19 @@ class profile::chocolatey (
 ) {
   include chocolatey
 
+  class {'chocolatey':
+    chocolatey_download_url       => $chocolatey_download_url,
+    use_7zip                      => false,
+    choco_install_timeout_seconds => 2700,
+  }
+
   chocolateysource {'chocolatey':
     ensure => disabled,
   }
 
   chocolateysource {'chocolatey':
     ensure   => present,
-    location => $location
+    location => $location,
     priority => 1,
   }
 }
