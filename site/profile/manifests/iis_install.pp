72b69f38411dab0146930b17e974b1b0bb63a38b
joeypiccola <joey.piccola@gmail.com>
2017-09-13 12:26:27 -0600

    added rest of features

--- site/profile/manifests/iis_install.pp
+++ site/profile/manifests/iis_install.pp
@@ -2,9 +2,9 @@
 class profile::iis_install (
 ) {
 
-  $iis_features = ['web-server','web-webserver','Web-Common-Http','Web-Static-Content','Web-Mgmt-Tools','Web-Mgmt-Console']
+  $iis_features = ['web-server','web-webserver','Web-Common-Http','Web-Static-Content','Web-Mgmt-Tools','Web-Mgmt-Console','Web-Windows-Auth','Web-Stat-Compression','Web-Dyn-Compression','Web-Dir-Browsing','Web-Http-Errors','Web-Http-Logging','Web-Default-Doc','Web-Asp-Net','Web-Includes','Web-Filtering','Web-Mgmt-Service','Web-Security','Web-Basic-Auth','Web-Performance','Web-Request-Monitor','Web-App-Dev','Web-Health']
   
   iis_feature { $iis_features:
-    ensure                   => 'present',
+    ensur => 'present',
   }
 }
