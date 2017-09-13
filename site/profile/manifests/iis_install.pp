510186097650eecb6785c23d3946f6d209ce04bb
joeypiccola <joey.piccola@gmail.com>
2017-09-13 11:20:47 -0600

    trying to figure out what iis_features can and cannot install

--- site/profile/manifests/iis_install.pp
+++ site/profile/manifests/iis_install.pp
@@ -2,7 +2,7 @@
 class profile::iis_install (
 ) {
 
-  $iis_features = ['IIS-WebServerRole','IIS-WebServer','IIS-CommonHttpFeatures','IIS-StaticContent','IIS-DefaultDocument','IIS-DirectoryBrowsing','IIS-HttpErrors','IIS-HttpRedirect','IIS-ApplicationDevelopment','IIS-ASPNET','IIS-ServerSideIncludes','IIS-HealthAndDiagnostics','IIS-HttpLogging','IIS-RequestMonitor','IIS-Security','IIS-BasicAuthentication','IIS-WindowsAuthentication','IIS-RequestFiltering','IIS-Performance','IIS-HttpCompressionStatic','IIS-HttpCompressionDynamic','IIS-WebServerManagementTools','IIS-ManagementService']
+  $iis_features = ['web-server','web-webserver','Web-Common-Http','Web-Static-Content']
   
   iis_feature { $iis_features:
     ensure => 'present',
