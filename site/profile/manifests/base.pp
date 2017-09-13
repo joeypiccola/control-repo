617163264bc6c97e52793cb6a27738fffc2469e6
joeypiccola <joey.piccola@gmail.com>
2017-09-12 21:19:30 -0600

    added internal choco support

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,17 +1,6 @@
 # == Class: profile::base
 class profile::base (
-  $packages,
 ) {
-  include chocolatey
-
-  Package {
-    ensure => latest,
-    provider => chocolatey,
-  }
-
-  # Dynamic installed packages (Defined in Heira)
-  each ($packages) | $package | {
-    package { $package: }
-  }
-    
+  include profile::chocolatey
+  include profile::packages
 }
