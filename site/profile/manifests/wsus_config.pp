6407daf96278d2011709316815b58f69e1b23004
joeypiccola <joey.piccola@gmail.com>
2017-09-15 11:20:43 -0600

    added env hiera support and datacenter wsus

--- site/profile/manifests/wsus_config.pp
+++ site/profile/manifests/wsus_config.pp
@@ -1,9 +1,10 @@
 # == Class: profile::wsus_config
 class profile::wsus_config (
+  $server_url
 ) {
 
   class { 'wsus_client':
-    server_url                => 'http://box.ad.piccola.us:8530',
+    server_url                => $server_url,
     enable_status_server      => true,
     auto_update_option        => 'AutoNotify',
     detection_frequency_hours => 1,
