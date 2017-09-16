b5b5a279b710e407746160d6df61b719160108f7
joeypiccola <joey.piccola@gmail.com>
2017-09-16 11:55:43 -0600

    removed camel case

--- site/profile/facts.d/WinActivationStatus.ps1
+++ site/profile/facts.d/WinActivationStatus.ps1
@@ -1,23 +1,23 @@
 switch ((Get-WmiObject -Class SoftwareLicensingProduct | Where-Object {$_.ApplicationID -eq "55c92734-d682-4d71-983e-d6ec3f16059f" -AND $_.PartialProductKey -ne $null}).LicenseStatus ) {
     '0' {
-        Write-Output 'WinActivationStatus=Unactivated'
+        Write-Output 'winactivationstatus=Unactivated'
     }
     '1' {
-        Write-Output 'WinActivationStatus=Activated'
+        Write-Output 'winactivationstatus=Activated'
     }
     '2' {
-        Write-Output 'WinActivationStatus=OOBGrace'
+        Write-Output 'winactivationstatus=OOBGrace'
     }
     '3' {
-        Write-Output 'WinActivationStatus=OOTGrace'
+        Write-Output 'winactivationstatus=OOTGrace'
     }
     '4' {
-        Write-Output 'WinActivationStatus=NonGenuineGrace'
+        Write-Output 'winactivationstatus=NonGenuineGrace'
     }
     '5' {
-        Write-Output 'WinActivationStatus=Notification'
+        Write-Output 'winactivationstatus=Notification'
     }
     '6' {
-        Write-Output 'WinActivationStatus=ExtendedGrace'
+        Write-Output 'winactivationstatus=ExtendedGrace'
     }
 }
