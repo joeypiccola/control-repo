8d0a319e24d3a5f5ebd23381e78245bf9e6d9d25
joeypiccola <joey.piccola@gmail.com>
2017-09-17 14:10:57 -0600

    adding some debuging

--- site/profile/manifests/nameservers.pp
+++ site/profile/manifests/nameservers.pp
@@ -6,6 +6,9 @@ class profile::nameservers (
   $net_interfaces = split($facts['interfaces'], ',')
 
   $net_interfaces.each |String $net_interface| {
+
+    notice("Current working net_interface is: ${net_interface}")
+
     dsc_xdnsserveraddress { $net_interface:
       dsc_address        => $nameservers,
       dsc_interfacealias => $net_interface,
