02aff13b10753a01ada63401e938dc3844c117a8
joeypiccola <joey.piccola@gmail.com>
2017-09-19 17:00:17 -0600

    using defaults

--- site/profile/manifests/firewall_config.pp
+++ site/profile/manifests/firewall_config.pp
@@ -3,11 +3,6 @@ class profile::firewall_config (
 ) {
 
   class { 'firewallprofile_win':
-    standard_profile     => 'disabled',
-    public_profile       => 'disabled',
-    domain_profile       => 'disabled',
-    service_status       => 'running',
-    service_startup_type => 'automatic',
   }
 
 }
