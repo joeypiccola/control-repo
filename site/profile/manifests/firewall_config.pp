ad38d7179ce60147fdbeb6cbdcaec9bd2d4ec3f8
joeypiccola <joey.piccola@gmail.com>
2017-09-19 16:28:32 -0600

    validated firewall module with conflicting settings

--- site/profile/manifests/firewall_config.pp
+++ site/profile/manifests/firewall_config.pp
@@ -7,7 +7,7 @@ class profile::firewall_config (
     public_profile       => 'disabled',
     domain_profile       => 'disabled',
     service_status       => 'running',
-    service_startup_type => 'disabled',
+    service_startup_type => 'enabled',
   }
 
 }
