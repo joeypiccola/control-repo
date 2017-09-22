70d359323d18ae2c209e983cede1425076fd8492
Joey Piccola <joey@joeypiccola.com>
2017-09-22 14:34:30 -0600

    anger

--- site/profile/manifests/master.pp
+++ site/profile/manifests/master.pp
@@ -5,7 +5,7 @@ class profile::master {
           hiera5_defaults =>  {"datadir" => "data", "data_hash" => "yaml_data"},
           hierarchy       =>  [
                                 {"name" =>  "Per-node data", "path"  =>  "nodes/%{trusted.certname}.yaml"},
-                                {"name" =>  "Per-datacenter defaults", "paths" =>  "datacenters/%{facts.datacenter}.yaml"},
+                                {"name" =>  "Per-datacenter defaults", "path" =>  "datacenters/%{facts.datacenter}.yaml"},
                                 {"name" =>  "Per-domain defaults", "path" =>  "domains/%{facts.win_domain}.yaml"},
                                 {"name" =>  "Default yaml file", "path" =>  "common.yaml"},
                               ],
