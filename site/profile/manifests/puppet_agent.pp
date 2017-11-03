0475b177a96d06d0ab355e012e1a0019b9707e61
joeypiccola <joey.piccola@gmail.com>
2017-11-03 11:38:43 -0600

    should really read what i write

--- site/profile/manifests/puppet_agent.pp
+++ site/profile/manifests/puppet_agent.pp
@@ -1,5 +1,7 @@
 # == Class: profile::puppet_agent
-
-class {'::puppet_agent':
-  package_version => '5.0.0',
+class profile::puppet_agent (
+) {
+    class { 'puppet_agent' :
+    package_version => '5.0.0',
+  }
 }
