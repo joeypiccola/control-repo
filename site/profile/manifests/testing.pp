ad8c4af52bda78c7bc42a08b75f84a96c1fe9d70
joeypiccola <joey.piccola@gmail.com>
2018-01-15 12:42:53 -0700

    moved testing exec calls to module

--- site/profile/manifests/testing.pp
+++ site/profile/manifests/testing.pp
@@ -7,23 +7,4 @@ class profile::testing (
     value => $value,
   }
 
-  file { 'puppet_win_psfile':
-    ensure => 'present',
-    source => 'puppet:///modules/puppet_win/Test-Param.ps1',
-    path   => 'c:/windows/temp/Test-Param.ps1',
-    before => Exec['puppet_win_psexec'],
-  }
-
-  exec { 'puppet_win_psexec':
-    command   => "& C:\\windows\\temp\\Test-Param.ps1 -Value ${value}",
-    provider  => 'powershell',
-    logoutput => true,
-  }
-
-  exec { 'puppet_win_psexec_file':
-    command   => file("puppet_win/Test-Param.ps1 -Value ${value}"),
-    provider  => 'powershell',
-    logoutput => true,
-  }
-
 }
