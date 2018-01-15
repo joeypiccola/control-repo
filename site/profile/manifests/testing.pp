66510dc11eaa99aa1436e63c3affa7ed4012756b
joeypiccola <joey.piccola@gmail.com>
2018-01-15 13:25:20 -0700

    added additional params

--- site/profile/manifests/testing.pp
+++ site/profile/manifests/testing.pp
@@ -1,12 +1,14 @@
 # == Class: profile::testing
 class profile::testing (
   $value,
+  $valuetwo,
   $pswindowsupdateurl,
   $wsusscnurl,
 ) {
 
   class { 'puppet_win':
     value              => $value,
+    valuetwo           => $valuetwo,
     pswindowsupdateurl => $pswindowsupdateurl,
     wsusscnurl         => $wsusscnurl,
   }
