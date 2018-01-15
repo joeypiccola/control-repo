0d1d8e884787a96dfd0163d8426b1824b957944d
joeypiccola <joey.piccola@gmail.com>
2018-01-15 11:40:20 -0700

    trying script

--- site/profile/manifests/testing.pp
+++ site/profile/manifests/testing.pp
@@ -6,4 +6,16 @@ class profile::testing (
     value => 'the value i want ps to output'
   }
 
+  file { 'puppet_win_psfile':
+    ensure => 'present',
+    source => 'puppet:///modules/puppet_win/Test-Param.ps1',
+    path   => 'c:/windows/temp',
+    before => Exec['puppet_win_psexec'],
+  }
+
+  exec { 'puppet_win_psexec':
+    command   => '& C:\windows\temp\Test-Param.ps1 -Value blahblah',
+    provider  => 'powershell',
+    logoutput => true,
+  }
 }
