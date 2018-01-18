5df6d567727e47d82483baa2aa84635827e28ef2
joeypiccola <joey.piccola@gmail.com>
2018-01-18 09:29:30 -0700

    removed schedule bc it does not work with classes

--- site/profile/manifests/testing.pp
+++ site/profile/manifests/testing.pp
@@ -5,17 +5,10 @@ class profile::testing (
   $wsusscnurl,
 ) {
 
-  schedule { 'scan':
-    range  => '6 - 12',
-    period => daily,
-    repeat => 1,
-  }
-
   class { 'puppet_win':
     pswindowsupdateurl => $pswindowsupdateurl,
     wsusscnurl         => $wsusscnurl,
     downloaddirectory  => $downloaddirectory,
-    schedule           => 'scan',
   }
 
 }
