b55bbc200f6666f76be049c3354d384bc1c8a24b
joeypiccola <joey.piccola@gmail.com>
2018-01-18 23:07:00 -0700

    added additional puppet_win params and values to hiera #2

--- site/profile/manifests/testing.pp
+++ site/profile/manifests/testing.pp
@@ -3,12 +3,22 @@ class profile::testing (
   $download_directory,
   $pswindowsupdate_url,
   $wsusscn_url,
+  $task_day_of_week,
+  $task_every,
+  $task_schedule,
+  $task_enabled,
+  $task_ensure,
 ) {
 
   class { 'puppet_win':
     pswindowsupdate_url => $pswindowsupdate_url,
     wsusscn_url         => $wsusscn_url,
     download_directory  => $download_directory,
+    task_day_of_week    => $task_day_of_week,
+    task_every          => $task_every,
+    task_ensure         => $task_ensure,
+    task_schedule       => $task_schedule,
+    task_enabled        => $task_enabled,
   }
 
 }
