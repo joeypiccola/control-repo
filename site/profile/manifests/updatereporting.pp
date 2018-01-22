a9dae1d3055dbddc84f8b7b9b6d0a8e674c6f99d
joeypiccola <joey.piccola@gmail.com>
2018-01-21 21:37:49 -0700

    added ps windows update module download

--- site/profile/manifests/updatereporting.pp
+++ site/profile/manifests/updatereporting.pp
@@ -2,6 +2,7 @@
 class profile::testing (
   $download_directory,
   $pswindowsupdate_url,
+  $pswindowsupdate_force_download,
   $wsusscn_url,
   $task_day_of_week,
   $task_every,
@@ -11,14 +12,15 @@ class profile::testing (
 ) {
 
   class { 'puppet_win':
-    pswindowsupdate_url => $pswindowsupdate_url,
-    wsusscn_url         => $wsusscn_url,
-    download_directory  => $download_directory,
-    task_day_of_week    => $task_day_of_week,
-    task_every          => $task_every,
-    task_ensure         => $task_ensure,
-    task_schedule       => $task_schedule,
-    task_enabled        => $task_enabled,
+    pswindowsupdate_url            => $pswindowsupdate_url,
+    pswindowsupdate_force_download => $pswindowsupdate_force_download,
+    wsusscn_url                    => $wsusscn_url,
+    download_directory             => $download_directory,
+    task_day_of_week               => $task_day_of_week,
+    task_every                     => $task_every,
+    task_ensure                    => $task_ensure,
+    task_schedule                  => $task_schedule,
+    task_enabled                   => $task_enabled,
   }
 
 }
