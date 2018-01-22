42925da0d1f3bc94340325f73345dcf8d0374dc4
joeypiccola <joey.piccola@gmail.com>
2018-01-21 21:35:22 -0700

    removed puppet_win

--- site/profile/manifests/testing.pp
+++ site/profile/manifests/testing.pp
@@ -1,24 +1,6 @@
 # == Class: profile::testing
 class profile::testing (
-  $download_directory,
-  $pswindowsupdate_url,
-  $wsusscn_url,
-  $task_day_of_week,
-  $task_every,
-  $task_schedule,
-  $task_enabled,
-  $task_ensure,
-) {
 
-  class { 'puppet_win':
-    pswindowsupdate_url => $pswindowsupdate_url,
-    wsusscn_url         => $wsusscn_url,
-    download_directory  => $download_directory,
-    task_day_of_week    => $task_day_of_week,
-    task_every          => $task_every,
-    task_ensure         => 'absent',
-    task_schedule       => $task_schedule,
-    task_enabled        => $task_enabled,
-  }
+) {
 
 }
