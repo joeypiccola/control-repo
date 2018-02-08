373b1ed11a870767afc14e989f92852cb94b0b2f
joeypiccola <joey.piccola@gmail.com>
2018-02-08 03:04:05 -0700

    reduced udpate reporting

--- site/profile/manifests/updatereporting.pp
+++ site/profile/manifests/updatereporting.pp
@@ -1,26 +1,9 @@
 # == Class: profile::updatereporting
 class profile::updatereporting (
-  $download_directory,
-  $pswindowsupdate_url,
-  $pswindowsupdate_force_download,
   $wsusscn_url,
-  $task_day_of_week,
-  $task_every,
-  $task_schedule,
-  $task_enabled,
-  $task_ensure,
 ) {
 
   class { 'updatereporting_win':
-    pswindowsupdate_url            => $pswindowsupdate_url,
-    pswindowsupdate_force_download => $pswindowsupdate_force_download,
-    wsusscn_url                    => $wsusscn_url,
-    download_directory             => $download_directory,
-    task_day_of_week               => $task_day_of_week,
-    task_every                     => $task_every,
-    task_ensure                    => $task_ensure,
-    task_schedule                  => $task_schedule,
-    task_enabled                   => $task_enabled,
+    wsusscn_url => $wsusscn_url,
   }
-
 }
