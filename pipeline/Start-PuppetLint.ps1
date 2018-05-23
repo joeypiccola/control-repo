e2949e800042b6b809769dd06e782c1e678980b5
joeypiccola <joey.piccola@gmail.com>
2018-05-23 00:23:10 -0600

    fixed loop comments

--- pipeline/Start-PuppetLint.ps1
+++ pipeline/Start-PuppetLint.ps1
@@ -5,8 +5,8 @@ $LintResults = @()
 foreach ($pp in (Get-ChildItem -path .\ -Recurse -Include *.pp)){
     Write-Information "Linting $($pp.FullName)"
     $LintResult = & puppet-lint $pp.FullName --with-filename --no-140chars-check
+    if ($LintResult) {$LintResult | %{Write-Information $_}}
     $LintResults += $LintResult
 }
 
-$LintResults | %{Write-Information $_}
 if ($LintResults | Select-String -Pattern 'error') {exit 1}
