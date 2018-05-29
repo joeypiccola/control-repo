437b610ef06bdbe4d2085f2bb6f679cb1f9241b5
Joey Piccola <joey@joeypiccola.com>
2018-05-29 08:58:21 -0600

    wsus changes

--- site/profile/manifests/wsus_config.pp
+++ site/profile/manifests/wsus_config.pp
@@ -3,10 +3,12 @@ class profile::wsus_config (
 ) {
 
   class { 'wsus_client':
-    auto_update_option        => "Scheduled",
-    scheduled_install_day     => "Tuesday",
-    scheduled_install_hour    => 2,
-    detection_frequency_hours => 6,
+    auto_update_option                  => "Scheduled",
+    scheduled_install_day               => "Tuesday",
+    scheduled_install_hour              => 2,
+    detection_frequency_hours           => 6,
+    no_auto_reboot_with_logged_on_users => false,
+    reboot_relaunch_timeout_minutes     => 2,
   }
 
 }
