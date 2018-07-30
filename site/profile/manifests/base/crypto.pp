b9a08791a82b182a7ca8d21426238a5afc8326c5
Joey Piccola <joey@joeypiccola.com>
2018-07-30 12:44:29 -0600

    revert to orig crypto pro

--- site/profile/manifests/base/crypto.pp
+++ site/profile/manifests/base/crypto.pp
@@ -2,13 +2,6 @@
 class profile::base::crypto (
 ) {
 
-  class { 'crypto_win':
-    notify => Registry_key['RebootPending']
-  }
-
-  registry_key { 'RebootPending':
-    ensure => present,
-    path   => 'HKLM\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending',
-  }
+  include crypto_win
 
 }
