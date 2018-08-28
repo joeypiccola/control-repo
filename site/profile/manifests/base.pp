61943509bc5e1c09ebccc9a72aaf3057f226955f
joeypiccola <joey.piccola@gmail.com>
2018-08-28 14:50:39 -0600

    add patching cleanup choco and rm djoin

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,19 +1,14 @@
 # == Class: profile::base
 class profile::base (
 ) {
-  include profile::base::choco_install
-  include profile::base::choco_config
-  include profile::base::choco_packages
+  include profile::base::chocolatey
   include profile::base::crypto
-  include profile::base::domain_join
   include profile::base::firewall_config
   include profile::base::kms
   include profile::base::nameservers
   include profile::base::timezone
   include profile::base::uac_config
   include profile::base::updatereporting
-  include profile::patching::install_saturday_config
-  include profile::patching::pswindowsupdate_config
-  include profile::patching::wsus_config
+  include profile::patching::patching
   include profile::puppet_agent
 }
