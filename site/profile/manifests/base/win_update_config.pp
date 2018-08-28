d8311d1614711172ac6566681943f744e5f6c000
joeypiccola <joey.piccola@gmail.com>
2018-08-27 23:08:08 -0600

    moving to ps

--- site/profile/manifests/base/win_update_config.pp
+++ site/profile/manifests/base/win_update_config.pp
@@ -2,13 +2,26 @@
 class profile::base::win_update_config (
 ) {
 
-  archive { 'pswindowsupdate':
-    ensure       => present,
-    extract      => true,
-    extract_path => 'C:/Windows/Temp',
-    source       => 'http://nuget.ad.piccola.us:8081/PSWindowsUpdate_2004.zip',
-    path         => 'C:/Windows/Temp/PSWindowsUpdate_2004',
-    cleanup      => true,
+  # archive { 'pswindowsupdate':
+  #   ensure       => present,
+  #   extract      => true,
+  #   extract_path => 'C:/Windows/Temp',
+  #   source       => 'http://nuget.ad.piccola.us:8081/PSWindowsUpdate_2004.zip',
+  #   path         => 'C:/Program Files/WindowsPowerShell/Modules',
+  #   cleanup      => true,
+  # }
+
+  exec { 'ps_pswindowsupdate':
+    command  => "Import-Module -Name BitsTransfer
+                 Start-BitsTransfer -Source 'http://nuget.ad.piccola.us:8081/PSWindowsUpdate_2004.zip' -Destination 'C:/Windows/Temp'
+                 7z e -spf 'c:/Windows/Temp/PSWindowsUpdate.zip' -o'C:/Program Files/WindowsPowerShell/Modules'",
+    onlyif   => "if (Get-Module -ListAvailable -Name 'PSWindowsUpdate') {
+                   exit 1
+                 } else {
+                   exit 0
+                 }",
+    provider => 'powershell',
   }
+
 
 }
