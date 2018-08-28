81624c03f58d5d8204b9b7dc632fdb01b1617994
joeypiccola <joey.piccola@gmail.com>
2018-08-27 23:54:57 -0600

    if fix

--- site/profile/manifests/base/win_update_config.pp
+++ site/profile/manifests/base/win_update_config.pp
@@ -15,7 +15,7 @@ class profile::base::win_update_config (
     command  => "Import-Module -Name 'BitsTransfer'
                  Start-BitsTransfer -Source 'http://nuget.ad.piccola.us:8081/PSWindowsUpdate.zip' -Destination 'C:/Windows/Temp'
                  7z e -spf 'c:/Windows/Temp/PSWindowsUpdate.zip' -o'C:/Program Files/WindowsPowerShell/Modules'",
-    onlyif   => "if ([string](Get-Module -ListAvailable -Name 'PSWindowsUpdate').version -eq '2.0.0.4' { exit 1 } else { exit 0 }",
+    onlyif   => "if ([string](Get-Module -ListAvailable -Name 'PSWindowsUpdate').version -eq '2.0.0.4') { 1 } else { 0 }",
     provider => 'powershell',
     require  => Package['7zip'],
   }
