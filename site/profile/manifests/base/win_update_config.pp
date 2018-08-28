8a1f1f83da5470cac6728068ef6514083cf877ce
joeypiccola <joey.piccola@gmail.com>
2018-08-27 22:17:42 -0600

    fixes for archive

--- site/profile/manifests/base/win_update_config.pp
+++ site/profile/manifests/base/win_update_config.pp
@@ -3,11 +3,11 @@ class profile::base::win_update_config (
 ) {
 
   archive { 'pswindowsupdate':
-    ensure  => present,
-    extract => true,
-    source  => 'http://nuget.ad.piccola.us:8081/PSWindowsUpdate_2004.zip',
-    creates => 'C:/Windows/Temp/PSWindowsUpdate_2004',
-    cleanup => true,
+    ensure       => present,
+    extract      => true,
+    extract_path => 'C:/Windows/Temp',
+    source       => 'http://nuget.ad.piccola.us:8081/PSWindowsUpdate_2004.zip',
+    cleanup      => false,
   }
 
 }
