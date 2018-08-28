8d447163f89ecce2bc2c1c558bc29a8203d41e95
joeypiccola <joey.piccola@gmail.com>
2018-08-28 08:10:31 -0600

    update wsus_client

--- site/profile/manifests/patching/wsus_config.pp
+++ site/profile/manifests/patching/wsus_config.pp
@@ -3,12 +3,13 @@ class profile::patching::wsus_config (
 ) {
 
   class { 'wsus_client':
-    auto_update_option                  => 'Scheduled',
-    scheduled_install_day               => 'Tuesday',
+    auto_update_option                  => 'AutoNotify',
     scheduled_install_hour              => 2,
     detection_frequency_hours           => 6,
     no_auto_reboot_with_logged_on_users => false,
     reboot_relaunch_timeout_minutes     => 2,
+    server_url                          => 'http://wsus.ad.piccola.us:8530',
+    target_group                        => 'Saturday Patch Group',
   }
 
 }
