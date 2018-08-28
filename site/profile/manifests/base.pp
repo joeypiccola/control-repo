6f938529b0bfeb99d5621d85c648051c2c196a96
joeypiccola <joey.piccola@gmail.com>
2018-08-28 00:32:21 -0600

    renames

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -5,14 +5,15 @@ class profile::base (
   include profile::base::choco_config
   include profile::base::choco_packages
   include profile::base::crypto
-  include profile::base::timezone
+  include profile::base::domain_join
+  include profile::base::firewall_config
   include profile::base::kms
   include profile::base::nameservers
+  include profile::base::timezone
   include profile::base::uac_config
-  include profile::base::firewall_config
-  include profile::base::domain_join
   include profile::base::updatereporting
-  include profile::base::wsus_config
+  include profile::patching::install_saturday_config
+  include profile::patching::pswindowsupdate_config
+  include profile::patching::wsus_config
   include profile::puppet_agent
-  include profile::base::win_update_config
 }
