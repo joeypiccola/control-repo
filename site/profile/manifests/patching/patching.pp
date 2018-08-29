60ceef6ba24d2047cbeb8e4b90fb840cf1538628
joeypiccola <joey.piccola@gmail.com>
2018-08-28 22:10:02 -0600

    mv ps winup choco pkg to profile

--- site/profile/manifests/patching/patching.pp
+++ site/profile/manifests/patching/patching.pp
@@ -3,7 +3,18 @@ class profile::patching::patching (
   Optional[String] $patchgroup = undef,
 ) {
 
-  include profile::patching::pswindowsupdate_config
+  include profile::base::chocolatey
+
+  package { '7zip':
+    ensure   => '18.5.0.20180730',
+    provider => 'chocolatey',
+  }
+
+  package { 'pswindowsupdate':
+    ensure   => '2.0.0.4',
+    provider => 'chocolatey',
+    require  => Package['7zip'],
+  }
 
   class { 'wsus_client':
     target_group => $patchgroup,
