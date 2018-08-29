5cc4b59dcf4dd4b5e8cacd978f046709ac79d67d
joeypiccola <joey.piccola@gmail.com>
2018-08-28 22:09:50 -0600

    mv to choc package  for pswin

--- site/profile/manifests/patching/pswindowsupdate_config.pp
+++ site/profile/manifests/patching/pswindowsupdate_config.pp
@@ -4,20 +4,15 @@ class profile::patching::pswindowsupdate_config (
 
   include profile::base::chocolatey
 
-  exec { 'pswindowsupdate':
-    command  => "Remove-Item 'C:/Program Files/WindowsPowerShell/Modules/PSWindowsUpdate' -Recurse -Force -ErrorAction SilentlyContinue
-                 Import-Module -Name 'BitsTransfer' -ErrorAction Stop
-                 Start-BitsTransfer -Source 'http://nuget.ad.piccola.us:8081/PSWindowsUpdate.zip' -Destination 'C:/Windows/Temp' -ErrorAction Stop
-                 7z e -spf 'c:/Windows/Temp/PSWindowsUpdate.zip' -o'C:/Program Files/WindowsPowerShell/Modules'
-                 Remove-Item -Recurse C:/Windows/Temp/PSWindowsUpdate.zip -ErrorAction Stop",
-    onlyif   => "if ([string](Get-Module -ListAvailable -Name 'PSWindowsUpdate').version -eq '2.0.0.4') { exit 1 } else { exit 0 }",
-    provider => 'powershell',
-    require  => Package['7zip'],
-  }
-
   package { '7zip':
     ensure   => '18.5.0.20180730',
     provider => 'chocolatey',
+  }
+
+  package { 'pswindowsupdate':
+    ensure   => '2.0.0.4',
+    provider => 'chocolatey',
+    require  => Package['7zip'],
   }
 
 }
