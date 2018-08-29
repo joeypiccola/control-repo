bcf42bf2e7d9297e860864ce01b962f71cf7247c
joeypiccola <joey.piccola@gmail.com>
2018-08-29 11:26:21 -0600

    rm nameservers bc dsc no work on lt ps 5

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -5,7 +5,6 @@ class profile::base (
   include profile::base::crypto
   include profile::base::firewall_config
   include profile::base::kms
-  include profile::base::nameservers
   include profile::base::timezone
   include profile::base::uac_config
   include profile::base::updatereporting
