32aa87981a27c3ce9499f614ae0ef7dab054db0d
joeypiccola <joey.piccola@gmail.com>
2018-08-30 21:46:13 -0600

    add logic so dsc only happens on ps5

--- site/profile/manifests/base/nameservers.pp
+++ site/profile/manifests/base/nameservers.pp
@@ -2,17 +2,17 @@
 class profile::base::nameservers (
   $nameservers,
 ) {
-
-  $net_interfaces = split($facts['interfaces'], ',')
-
-  $net_interfaces.each |String $net_interface| {
-
-    notice("Current working net_interface is: ${net_interface}")
-
-    dsc_xdnsserveraddress { $net_interface:
-      dsc_address        => $nameservers,
-      dsc_interfacealias => $net_interface,
-      dsc_addressfamily  => 'IPv4',
+  # if we're on ps v5 then lets do stuff with DSC!
+  $psmajor = split($facts['powershell_version'], '[.]')
+  if $psmajor[0] == 5 {
+    $net_interfaces = split($facts['interfaces'], ',')
+    $net_interfaces.each |String $net_interface| {
+      notice("Current working net_interface is: ${net_interface}")
+      dsc_xdnsserveraddress { $net_interface:
+        dsc_address        => $nameservers,
+        dsc_interfacealias => $net_interface,
+        dsc_addressfamily  => 'IPv4',
+      }
     }
   }
 }
