c9ac26e4b5f4d4b9ada7111098204c3cfcab2707
joeypiccola <joey.piccola@gmail.com>
2018-08-31 14:22:42 -0600

    maybe

--- site/profile/manifests/base/nameservers.pp
+++ site/profile/manifests/base/nameservers.pp
@@ -4,7 +4,8 @@ class profile::base::nameservers (
 ) {
   # if we're on ps v5 then lets do stuff with DSC!
   $psmajor = split($facts['powershell_version'], '[.]')
-  if $psmajor[0] == '5' {
+  $osmajor = $facts['os']['release']['major']
+  if ($psmajor[0]) == '5' and !('2008' in $osmajor) {
     $net_interfaces = split($facts['interfaces'], ',')
     $net_interfaces.each |String $net_interface| {
       notice("Current working net_interface is: ${net_interface}")
