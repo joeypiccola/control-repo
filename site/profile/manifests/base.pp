5408b61669be6ecda97fb72220c7dbfd3fb82d05
joeypiccola <joey.piccola@gmail.com>
2018-08-30 21:47:19 -0600

    add ps and name server to base

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -6,6 +6,8 @@ class profile::base (
   include profile::base::firewall_config
   include profile::base::kms
   include profile::base::notify
+  include profile::base::powershell
+  include profile::base::nameservers
   include profile::base::timezone
   include profile::base::uac_config
   include profile::base::updatereporting
