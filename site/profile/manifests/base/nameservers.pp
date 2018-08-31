c4c406f0e95a5433f5c95975520f374f9c1f489a
joeypiccola <joey.piccola@gmail.com>
2018-08-31 15:46:04 -0600

    better comments

--- site/profile/manifests/base/nameservers.pp
+++ site/profile/manifests/base/nameservers.pp
@@ -2,7 +2,7 @@
 class profile::base::nameservers (
   $nameservers,
 ) {
-  # if we're on ps v5 then lets do stuff with DSC!
+  # if we're on ps v5 and not 2008 then lets do stuff with DSC!
   $psmajor = split($facts['powershell_version'], '[.]')
   $osmajor = $facts['os']['release']['major']
   if ($psmajor[0]) == '5' and !('2008' in $osmajor) {
