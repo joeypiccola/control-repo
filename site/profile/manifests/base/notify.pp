a3ce0c424ca30588cb9f1db86fb0d29c9b31cc18
joeypiccola <joey.piccola@gmail.com>
2018-08-30 21:35:36 -0600

    more puppet learning

--- site/profile/manifests/base/notify.pp
+++ site/profile/manifests/base/notify.pp
@@ -4,16 +4,16 @@ class profile::base::notify (
 
 
   case $facts['powershell_version'] {
-    /^5\./: {
-      notify { 'ps_five':
-        name    => '5 name',
-        message => 'ps 5 detected',
-      }
-    }
-    /^4\./: {
+    # /^5\./: {
+    #   notify { 'ps_five':
+    #     name    => '5 name',
+    #     message => 'ps 5 detected',
+    #   }
+    # }
+    /^4\.|5\./: {
       notify { 'ps_four':
-        name    => '4 name',
-        message => 'ps 4 detected',
+        name    => '4 or 5 name',
+        message => 'ps 4 or 5 detected',
       }
     }
     /^3\./: {
@@ -30,5 +30,9 @@ class profile::base::notify (
     }
   }
 
-
+  $psmajor = split($facts['powershell_version'], '[.]')
+  notify { 'psmajor':
+    name    => 'maybe this is the version of powershell.major',
+    message => $psmajor[0],
+  }
 }
