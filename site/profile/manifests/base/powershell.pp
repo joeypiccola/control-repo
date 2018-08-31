4fcfeed254faca67fa8ec7ae1a54a5cff6975b23
joeypiccola <joey.piccola@gmail.com>
2018-08-30 21:47:27 -0600

    logic to deal with ps versions

--- site/profile/manifests/base/powershell.pp
+++ site/profile/manifests/base/powershell.pp
@@ -1,13 +1,40 @@
 # == Class: profile::base::powershell
 class profile::base::powershell (
+  $powershell_upgrade_reboot = undef,
+  $upgrade = undef
 ) {
 
-  include profile::base::chocolatey
-
-  package { 'powershell':
-    ensure   => '5.1.14409.20180105',
-    provider => 'chocolatey',
-
+  if $upgrade == true {
+    require profile::base::chocolatey
+    case $facts['powershell_version'] {
+      /^4\.|5\./: {
+        # this is where we begin management of powershell.
+        # if we're on some version of 5 the ensure we're on the following version.
+        package { 'powershell5':
+          ensure   => '5.1.14409.20180811',
+          name     => 'powershell',
+          provider => 'chocolatey',
+        }
+      }
+      /^3\./: {
+        package { 'powershell3':
+          ensure   => '4.0.20141001',
+          name     => 'powershell',
+          provider => 'chocolatey',
+        }
+      }
+      default: {
+        notify { 'noMatch':
+          name    => 'no match',
+          message => 'no match',
+        }
+      }
+    }
+    if $powershell_upgrade_reboot == true {
+      reboot { 'powershell_upgrade_reboot':
+        subscribe => [Package['powershell5'],Package['powershell3']]
+      }
+    }
   }
 
 }
