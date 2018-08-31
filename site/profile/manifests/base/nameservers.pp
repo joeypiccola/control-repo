01589777216da01f0dc685c79cf8b6eac64e67ac
joeypiccola <joey.piccola@gmail.com>
2018-08-31 13:58:32 -0600

    quote on int to conform string comparision

--- site/profile/manifests/base/nameservers.pp
+++ site/profile/manifests/base/nameservers.pp
@@ -4,7 +4,7 @@ class profile::base::nameservers (
 ) {
   # if we're on ps v5 then lets do stuff with DSC!
   $psmajor = split($facts['powershell_version'], '[.]')
-  if $psmajor[0] == 5 {
+  if $psmajor[0] == '5' {
     $net_interfaces = split($facts['interfaces'], ',')
     $net_interfaces.each |String $net_interface| {
       notice("Current working net_interface is: ${net_interface}")
