70c2105966750598621b0d96b359d38ca4db53d6
joeypiccola <joey.piccola@gmail.com>
2018-09-02 21:14:13 -0600

    force ExecutionTimeLimit

--- site/profile/manifests/patching/patching.pp
+++ site/profile/manifests/patching/patching.pp
@@ -43,6 +43,26 @@ class profile::patching::patching (
       minutes_interval => 1,
       minutes_duration => 5,
     }]
+    notify        => Exec['task_ExecutionTimeLimit']
   }
 
+  exec { 'task_ExecutionTimeLimit':
+    provider    => 'powershell',
+    command     => '$taskName = "Windows Update (Puppet Managed Scheduled Task)"
+                    $scheduler = New-Object -ComObject Schedule.Service
+                    $scheduler.Connect($null, $null, $null, $null)
+                    $taskFolder = $scheduler.GetFolder("")
+                    $task = $taskFolder.GetTask($taskName).Definition
+                    $task.Settings.ExecutionTimeLimit="PT6H"
+                    $taskFolder.RegisterTaskDefinition($taskName, $task, 4, $null, $null, 3) | Out-Null',
+    onlyif      => '$taskName = "Windows Update (Puppet Managed Scheduled Task)"
+                    $scheduler = New-Object -ComObject Schedule.Service
+                    $scheduler.Connect($null, $null, $null, $null)
+                    $taskFolder = $scheduler.GetFolder("")
+                    $task = $taskFolder.GetTask($taskName).Definition
+                    if ($task.Settings.ExecutionTimeLimit -eq "PT6H" {
+                      exit 1
+                    }',
+    refreshonly => true,
+  }
 }
