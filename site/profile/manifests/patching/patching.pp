d876f8eaa0c09d304a0bb3eae37e090c95952921
joeypiccola <joey.piccola@gmail.com>
2018-09-04 13:56:37 -0600

    move to hiera array

--- site/profile/manifests/patching/patching.pp
+++ site/profile/manifests/patching/patching.pp
@@ -1,7 +1,7 @@
 # == Class: profile::patching::patching
 class profile::patching::patching (
   Optional[String] $patchgroup = 'Undefined Patch Group',
-  Optional[String] $notkbarticleid = '',
+  Optional[Array] $notkbarticleid = [],
 ) {
 
   require profile::base::chocolatey
@@ -29,18 +29,26 @@ class profile::patching::patching (
     refreshonly => true,
   }
 
+  # nasty nasty
+  if $notkbarticleid.length > 0 {
+    $csv = join($notkbarticleid,',')
+    $notkbarticleid_param = "-NotKBArticleID ${csv}"
+  } else {
+    $notkbarticleid_param = ''
+  }
+
   scheduled_task { 'windows_update':
     ensure        => present,
     name          => 'Windows Update (Puppet Managed Scheduled Task)',
     enabled       => true,
     command       => 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe',
-    arguments     => "-WindowStyle Hidden -ExecutionPolicy Bypass \"c:/new-file.ps1 -NotKBArticleID ${notkbarticleid}\"",
+    arguments     => "-WindowStyle Hidden -ExecutionPolicy Bypass \"c:/new-file.ps1 ${notkbarticleid_param}\"",
     provider      => 'taskscheduler_api2',
     user          => 'system',
     trigger       => [{
       schedule         => 'daily',
       every            => 1,
-      start_time       => '13:30',
+      start_time       => '13:48',
       minutes_interval => 1,
       minutes_duration => 3,
     }],
