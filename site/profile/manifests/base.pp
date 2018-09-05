c69982c8b92ac1363825ef1617223e1eab8f7a0e
joeypiccola <joey.piccola@gmail.com>
2018-09-04 19:59:45 -0600

    rm notify profile

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -6,7 +6,6 @@ class profile::base (
   include profile::base::firewall_config
   include profile::base::kms
   include profile::base::localaccounts
-  include profile::base::notify
   include profile::base::powershell
   include profile::base::nameservers
   include profile::base::timezone
