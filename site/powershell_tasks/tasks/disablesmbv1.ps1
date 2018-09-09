b1297e1aac4db08e9a14421a65559a67a51b75f4
joeypiccola <joey.piccola@gmail.com>
2018-09-09 14:54:46 -0600

    really

--- site/powershell_tasks/tasks/disablesmbv1.ps1
+++ site/powershell_tasks/tasks/disablesmbv1.ps1
@@ -1,11 +1,3 @@
-Param (
-    [Parameter(Mandatory=$True)]
-    [ValidateSet('get', 'set')]
-    [string]$action
-)
-
-$ErrorActionPreference = 'Stop'
-
 switch ($action) {
     'get' {
         switch -Regex ((Get-WmiObject -Class win32_operatingsystem).version) {
@@ -34,31 +26,6 @@ switch ($action) {
         Write-Output ($test | ConvertTo-Json)
     }
     'set' {
-        try {
-            switch -Regex ((Get-WmiObject -Class win32_operatingsystem).version) {
-                '6.1' {
-                    # does the key  exist?
-                    if ($key = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters\' -name 'smb1' -ErrorAction SilentlyContinue) {
-                        # yes. if smb1 is enabled then disable it
-                        if ($key.smb1 -eq 1) {
-                            Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" SMB1 -Type DWORD -Value 0 –Force
-                        }
-                    } else {
-                        # no. the key does not exist, assume smb1 is enabled and disable it
-                        Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" SMB1 -Type DWORD -Value 0 –Force
-                    }
-                }
-                Default {
-                    # if smb1 is enabled then disable it
-                    if (Get-SmbServerConfiguration | Select-Object EnableSMB1Protocol) {
-                        Set-SmbServerConfiguration -EnableSMB1Protocol $false -Confirm:$false
-                    }
-                    # remove feature for good measure
-                    Remove-WindowsFeature -Name fs-smb1 -Confirm:$false
-                }
-            }
-        } catch {
-            Write-Error $_.Exception.Message
-        }
+        Write-Host 'set'
     }
 }
