c492a208078a0eca0bf098c8076f5fe886e7dedb
joeypiccola <joey.piccola@gmail.com>
2018-09-09 14:17:58 -0600

    rm try catch and force error stop

--- site/powershell_tasks/tasks/disablesmbv1.ps1
+++ site/powershell_tasks/tasks/disablesmbv1.ps1
@@ -4,6 +4,8 @@ Param (
     [string]$action
 )
 
+$ErrorActionPreference = 'Stop'
+
 switch ($action) {
     'test' {
         switch -Regex ((Get-WmiObject -Class win32_operatingsystem).version) {
@@ -29,11 +31,11 @@ switch ($action) {
                 }
             }
         }
-        Write-Output ($test | ConvertTo-Json)
+        Write-Outout ($test | ConvertTo-Json)
     }
     'set' {
-        try {
-            if ((Get-WmiObject -Class win32_operatingsystem).version -like '6.1*') {
+        switch -Regex ((Get-WmiObject -Class win32_operatingsystem).version) {
+            '6.1' {
                 # does the key  exist?
                 if ($key = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters\' -name 'smb1' -ErrorAction SilentlyContinue) {
                     # yes. if smb1 is enabled then disable it
@@ -44,7 +46,8 @@ switch ($action) {
                     # no. the key does not exist, assume smb1 is enabled and disable it
                     Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" SMB1 -Type DWORD -Value 0 –Force
                 }
-            } else {
+            }
+            Default {
                 # if smb1 is enabled then disable it
                 if (Get-SmbServerConfiguration | Select-Object EnableSMB1Protocol) {
                     Set-SmbServerConfiguration -EnableSMB1Protocol $false -Confirm:$false
@@ -52,8 +55,6 @@ switch ($action) {
                 # remove feature for good measure
                 Remove-WindowsFeature -Name fs-smb1 -Confirm:$false
             }
-        } catch {
-            Write-Error $_.Exception.Message
         }
     }
 }
