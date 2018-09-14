a510a135a16701b7c6775602eff4191086d62218
joeypiccola <joey.piccola@gmail.com>
2018-09-14 15:25:48 -0600

    pssa fixes

--- site/powershell_tasks/tasks/ps1exec.ps1
+++ site/powershell_tasks/tasks/ps1exec.ps1
@@ -1,3 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingInvokeExpression", "", Justification="I'll do it if want")]
+
 Param(
     [Parameter(Mandatory = $True)]
     [string]$Command
