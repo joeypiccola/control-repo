687ba0c9ebb33b73f1669209fcb7fe22e2af0e75
joeypiccola <joey.piccola@gmail.com>
2018-09-27 12:08:14 -0600

    fixes

--- site/profile/manifests/fileserver.pp
+++ site/profile/manifests/fileserver.pp
@@ -2,7 +2,7 @@
 class profile::fileserver (
 ) {
 
-  $windowsfeatures_present = ['FS-FileServer','FS-Resource-Manager']
+  $windowsfeatures_present = ['FS-FileServer','FS-Resource-Manager','RSAT-FSRM-Mgmt','RSAT-CoreFile-Mgmt']
   $windowsfeatures_absent = ['FS-SMB1']
 
   windowsfeature { $windowsfeatures_present:
