35b03b7a9f8d3f64e09bb60640a6985c71e1f3a3
joeypiccola <joey.piccola@gmail.com>
2018-09-27 12:14:40 -0600

    fix feature on 12

--- site/profile/manifests/fileserver.pp
+++ site/profile/manifests/fileserver.pp
@@ -2,7 +2,9 @@
 class profile::fileserver (
 ) {
 
-  $windowsfeatures_present = ['FS-FileServer','FS-Resource-Manager','RSAT-FSRM-Mgmt','RSAT-CoreFile-Mgmt']
+  # ugh, you need to assign present and not present based on the OS (08 vs 12)
+
+  $windowsfeatures_present = ['FS-FileServer','FS-Resource-Manager','RSAT-FSRM-Mgmt']
   $windowsfeatures_absent = ['FS-SMB1']
 
   windowsfeature { $windowsfeatures_present:
