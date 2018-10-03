5c9e5cff80a3246071de159a7fa302082d15dd2e
joeypiccola <joey.piccola@gmail.com>
2018-10-03 01:03:49 -0600

    add fs for demo

--- site/profile/manifests/fileserver.pp
+++ site/profile/manifests/fileserver.pp
@@ -2,10 +2,9 @@
 class profile::fileserver (
 ) {
 
-  # ugh, you need to assign present and not present based on the OS (08 vs 12)
-
   $windowsfeatures_present = ['FS-FileServer','FS-Resource-Manager','RSAT-FSRM-Mgmt']
-  $windowsfeatures_absent = ['FS-SMB1']
+  $windowsfeatures_absent  = ['FS-DFS-Namespace','FS-DFS-Replication']
+  # DEMO: 'FS-SMB1'
 
   windowsfeature { $windowsfeatures_present:
     ensure => present,
