30ca3a3249bfb7d7abd9a22f8987c8aca2f96868
joeypiccola <joey.piccola@gmail.com>
2018-10-23 16:07:57 -0600

    add ad fact

---
+++ site/profile/facts.d/admeta.ps1
@@ -0,0 +1,20 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseConsistentWhitespace", "", Justification = "just can't fix this")]
+$directorySearcher = New-Object System.DirectoryServices.DirectorySearcher
+$directorySearcher.Filter = "(&(objectCategory=Computer)(Name=$env:ComputerName))"
+$searcherPath = $directorySearcher.FindOne()
+$getDirectoryEntry = $searcherPath.GetDirectoryEntry()
+
+
+$dn = $getDirectoryEntry.distinguishedName
+$compobj = [PSCustomObject]@{
+    dn = $getDirectoryEntry.distinguishedName.ToString()
+    ou = $dn.substring(($dn.split(',')[0].length + 1), ($dn.Length - ($dn.split(',')[0].length + 1)))
+    whenCreated = $getDirectoryEntry.whenCreated.ToString()
+    whenChanged = $getDirectoryEntry.whenChanged.ToString()
+}
+
+$adobj = [PSCustomObject]@{
+    activedirectory_meta = $compobj
+}
+
+Write-Output ($adobj | ConvertTo-Json)
