cafa74988de2f937c9ed5892bbeed9a78b80cb24
Joey Piccola <joey.piccola@gmail.com>
2019-04-15 11:31:46 -0600

    added fix for PUP-9337

---
+++ site/profile/manifests/base/puppet_six_fixes.pp
@@ -0,0 +1,31 @@
+# == Class: profile::base::puppet_six_fixes
+class profile::base::puppet_six_fixes (
+) {
+
+  acl { 'C:/ProgramData/Puppetlabs/pxp-agent/etc/pxp-agent.conf':
+    group                      => 'S-1-5-21-3043862061-291277752-2514396928-513',
+    inherit_parent_permissions => false,
+    owner                      => 'S-1-5-32-544',
+    permissions                => [
+      {'identity'                => 'BUILTIN\Administrators', 'rights' => ['mask_specific'], 'mask' => '2032031', 'affects' => 'self_only'},
+      {'identity'                => 'AD\Domain Users', 'rights' => ['write', 'read'], 'affects' => 'self_only'},
+      {'identity'                => 'Everyone', 'rights' => ['mask_specific'], 'mask' => '1179776', 'affects' => 'self_only'},
+      {'identity'                => 'NT AUTHORITY\SYSTEM', 'rights' => ['full'], 'affects' => 'self_only'}
+    ],
+    require                    => File['C:/ProgramData/PuppetLabs/pxp-agent/etc/pxp-agent.conf'],
+  }
+
+  acl { 'C:/ProgramData/PuppetLabs/puppet/cache/state/package_inventory_enabled':
+    group                      => 'S-1-5-21-3043862061-291277752-2514396928-513',
+    inherit_parent_permissions => false,
+    owner                     => 'S-1-5-32-544',
+    permissions            => [
+      {'identity'               => 'BUILTIN\Administrators', 'rights' => ['mask_specific'], 'mask' => '2032031', 'affects' => 'self_only'},
+      {'identity'               => 'AD\Domain Users', 'rights' => ['write', 'read'], 'affects' => 'self_only'},
+      {'identity'               => 'Everyone', 'rights' => ['read'], 'affects' => 'self_only'},
+      {'identity'               => 'NT AUTHORITY\SYSTEM', 'rights' => ['full'], 'affects' => 'self_only'}
+    ],
+    require                   => File['C:/ProgramData/PuppetLabs/puppet/cache/state/package_inventory_enabled'],
+  }
+
+}
