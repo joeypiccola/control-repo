e0aaaf508c49299754ff69a84e9a529044230853
Joey Piccola <joey.piccola@gmail.com>
2019-04-27 22:22:44 -0600

    pssa fixes

--- site/powershell_tasks/tasks/account_audit.ps1
+++ site/powershell_tasks/tasks/account_audit.ps1
@@ -1,13 +1,13 @@
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWMICmdlet", "", Justification = "I'll do it if want")]
 
-$admins = Get-WmiObject -Class win32_groupuser | Where-Object {$_.groupcomponent –like '*"Administrators"'}
+$admins = Get-WmiObject -Class win32_groupuser | Where-Object { $_.groupcomponent -like '*"Administrators"' }
 
 $users = @()
 # omg not my code
 $admins | ForEach-Object {
-    $_.partcomponent –match “.+Domain\=(.+)\,Name\=(.+)$” > $nul
+    $_.partcomponent -match ".+Domain\=(.+)\,Name\=(.+)$" > $nul
     $match = $null
-    $match = $matches[1].trim('"') + “\” + $matches[2].trim('"')
+    $match = $matches[1].trim('"') + "\" + $matches[2].trim('"')
     $users += $match
 }
 
