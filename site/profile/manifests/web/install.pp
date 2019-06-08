ae9df08d74791173e9075e1f2f8391919051a43c
Joey Piccola <joey.piccola@gmail.com>
2019-06-08 16:17:25 -0600

    bad features maybe

--- site/profile/manifests/web/install.pp
+++ site/profile/manifests/web/install.pp
@@ -6,7 +6,9 @@ class profile::web::install (
   if ($iis) {
 
     # install IIS
-    $iis_features = ['web-server','web-webserver','Web-Common-Http','Web-Static-Content','Web-Mgmt-Tools','Web-Mgmt-Console','Web-Windows-Auth','Web-Stat-Compression','Web-Dyn-Compression','Web-Dir-Browsing','Web-Http-Errors','Web-Http-Logging','Web-Default-Doc','Web-Asp-Net','Web-Includes','Web-Filtering','Web-Mgmt-Service','Web-Security','Web-Basic-Auth','Web-Performance','Web-Request-Monitor','Web-App-Dev','Web-Health','Web-Http-Redirect']
+    $iis_features_old = ['web-server','web-webserver','Web-Common-Http','Web-Static-Content','Web-Mgmt-Tools','Web-Mgmt-Console','Web-Windows-Auth','Web-Stat-Compression','Web-Dyn-Compression','Web-Dir-Browsing','Web-Http-Errors','Web-Http-Logging','Web-Default-Doc','Web-Asp-Net','Web-Includes','Web-Filtering','Web-Mgmt-Service','Web-Security','Web-Basic-Auth','Web-Performance','Web-Request-Monitor','Web-App-Dev','Web-Health','Web-Http-Redirect']
+    $iis_features = ['web-webserver','Web-Asp-Net']
+
     iis_feature { $iis_features:
       ensure => 'present',
     }
