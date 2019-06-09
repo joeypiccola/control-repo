e6b1aef10e6fc0d38815274f4d8b840aea5cc809
Joey Piccola <joey.piccola@gmail.com>
2019-06-08 22:19:02 -0600

    erg

--- site/profile/manifests/web/install.pp
+++ site/profile/manifests/web/install.pp
@@ -7,7 +7,7 @@ class profile::web::install (
 
     # install IIS
     #$iis_features = ['web-server','web-webserver','Web-Common-Http','Web-Static-Content','Web-Mgmt-Tools','Web-Mgmt-Console','Web-Windows-Auth','Web-Stat-Compression','Web-Dyn-Compression','Web-Dir-Browsing','Web-Http-Errors','Web-Http-Logging','Web-Default-Doc','Web-Asp-Net','Web-Includes','Web-Filtering','Web-Mgmt-Service','Web-Security','Web-Basic-Auth','Web-Performance','Web-Request-Monitor','Web-App-Dev','Web-Health','Web-Http-Redirect']
-    $iis_features = ['web-webserver','Web-Asp-Net45','Web-AppInit','Web-Mgmt-Tools','Web-Mgmt-Console','']
+    $iis_features = ['web-webserver','Web-Asp-Net45','Web-AppInit','Web-Mgmt-Tools','Web-Mgmt-Console']
 
     iis_feature { $iis_features:
       ensure => 'present',
