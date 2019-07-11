3249dd830117f3c4400c12e4dd174b957add7209
Joey Piccola <joey.piccola@gmail.com>
2019-07-11 10:35:22 -0600

    bytes, doh

--- site/profile/manifests/base/eventlog.pp
+++ site/profile/manifests/base/eventlog.pp
@@ -3,17 +3,17 @@ class profile::base::eventlog (
 ) {
 
   windows_eventlog { 'Application':
-    log_size       => '61440',
+    log_size       => '62914560',
     max_log_policy => 'overwrite'
   }
 
   windows_eventlog { 'Security':
-    log_size       => '61440',
+    log_size       => '62914560',
     max_log_policy => 'overwrite'
   }
 
   windows_eventlog { 'System':
-    log_size       => '61440',
+    log_size       => '62914560',
     max_log_policy => 'overwrite'
   }
 
