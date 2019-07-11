d520bb3f7464b10e7dc8593322942f318f1773ff
Joey Piccola <joey.piccola@gmail.com>
2019-07-11 16:48:07 -0600

    mv site fact tolowercase

--- site/profile/facts.d/admeta.ps1
+++ site/profile/facts.d/admeta.ps1
@@ -11,16 +11,15 @@ if ($DomainRole -notmatch '^(0|2)') {
     $directorySearcher.Filter = "(&(objectCategory=Computer)(Name=$env:ComputerName))"
     $searcherPath = $directorySearcher.FindOne()
     $getDirectoryEntry = $searcherPath.GetDirectoryEntry()
-    $adsite = [System.DirectoryServices.ActiveDirectory.ActiveDirectorySite]::GetComputerSite().Name
 
     # make the results pretty
     $dn = $getDirectoryEntry.distinguishedName
     $compobj = [PSCustomObject]@{
-        dn          = $getDirectoryEntry.distinguishedName.ToString()
-        ou          = $dn.substring(($dn.split(',')[0].length + 1), ($dn.Length - ($dn.split(',')[0].length + 1)))
+        dn          = $getDirectoryEntry.distinguishedName.ToString().tolower()
+        ou          = $dn.substring(($dn.split(',')[0].length + 1), ($dn.Length - ($dn.split(',')[0].length + 1))).tolower()
         whenCreated = $getDirectoryEntry.whenCreated.ToString()
         whenChanged = $getDirectoryEntry.whenChanged.ToString()
-        site        = $adsite
+        site        = [System.DirectoryServices.ActiveDirectory.ActiveDirectorySite]::GetComputerSite().Name.ToLower()
     }
     $adobj = [PSCustomObject]@{
         activedirectory_meta = $compobj
