29518ae52c8b4b5a2d81d6845b1b1f006c6e4c97
Joey Piccola <joey.piccola@gmail.com>
2019-07-24 12:10:11 -0600

    rm files

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -5,11 +5,9 @@ class profile::base (
   include profile::base::chocolatey
   include profile::base::crypto
   include profile::base::eventlog
-  include profile::base::files
   include profile::base::firewall_config
   include profile::base::kms
   include profile::base::localaccounts
-  include profile::base::puppet_log
   #include profile::base::nameservers
   include profile::base::rdp_config
   include profile::base::timezone
