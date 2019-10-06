3fce6a2ac6d372d0de6c418a51ca6d31cc353520
Joey Piccola <joey.piccola@gmail.com>
2019-10-06 13:25:57 -0600

    add update reporting back in

--- site/profile/manifests/base/updatereporting.pp
+++ site/profile/manifests/base/updatereporting.pp
@@ -1,10 +1,5 @@
 # == Class: profile::base::updatereporting
 class profile::base::updatereporting (
-  $wsusscn_url,
 ) {
-
-  class { 'updatereporting_win':
-    wsusscn_url => $wsusscn_url,
-  }
-
+  include updatereporting_win
 }
