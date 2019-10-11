ec05af52a9a6cc50009e745a95b3a827ebc59e7d
Joey Piccola <joey.piccola@gmail.com>
2019-10-10 20:58:48 -0700

    take '2.0', make it '2', and then force it to 2 (i.e. an integer)

--- site/profile/manifests/base/updatereporting.pp
+++ site/profile/manifests/base/updatereporting.pp
@@ -1,8 +1,9 @@
 # == Class: profile::base::updatereporting
 class profile::base::updatereporting (
 ) {
+  # take '2.0', make it '2', and then force it to 2 (i.e. an integer)
   $psmajor = 0 + (split($facts['powershell_version'], '[.]'))[0]
-  if $psmajor[0] > 2 {
+  if $psmajor > 2 {
     include updatereporting_win
   }
 }
