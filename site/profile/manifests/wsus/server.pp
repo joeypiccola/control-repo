861e1d0719344b9b5e67dd7230695ceb19945c1e
Joey Piccola <joey.piccola@gmail.com>
2019-10-16 13:01:50 -0600

    wsus downstream support

--- site/profile/manifests/wsus/server.pp
+++ site/profile/manifests/wsus/server.pp
@@ -2,8 +2,12 @@
 
 class profile::wsus::server (
 ) {
+  # include wsusserver module
   include wsusserver
-  wsusserver_computer_target_group { ['Development', 'Staging', 'Production']:
-      ensure => 'present',
+  # if an upstream wsus server include the target_groups defined type
+  if $facts['application_component'] == 'upstream' {
+    wsusserver_computer_target_group { ['Development', 'Staging', 'Production']:
+        ensure => 'present',
+    }
   }
 }
