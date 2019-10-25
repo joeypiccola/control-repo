b8cb6f0737d4db2b7c849957e0260a2c984f4513
Joey Piccola <joey.piccola@gmail.com>
2019-10-24 21:57:39 -0600

    fix indent

--- site/powershell_tasks/tasks/wu_lastpatch.ps1
+++ site/powershell_tasks/tasks/wu_lastpatch.ps1
@@ -2,15 +2,15 @@
 $installedUpdates = Get-HotFix
 # get and format some useful installed update data
 $dateOfLastUpdateInstall = ($installedUpdates | Sort-Object -Property InstalledOn)[-1].InstalledOn
-$updatesLastInstalled = @($installedUpdates | Where-Object {$_.InstalledOn -eq $dateOfLastUpdateInstall} | ForEach-Object {$_.HotFixID})
+$updatesLastInstalled    = @($installedUpdates | Where-Object {$_.InstalledOn -eq $dateOfLastUpdateInstall} | ForEach-Object {$_.HotFixID})
 # get and format some useful system data
 $last_boot_time = (Get-CimInstance Win32_OperatingSystem).LastBootUpTime.ToString("G")
 # make it pretty
 $update_meta = [PSCustomObject]@{
-    installed_update_count      = $installedUpdates.count
-    last_update_install = "$dateOfLastUpdateInstall" # string needed for proper json
-    updates_last_installed      = $updatesLastInstalled
-    last_boot_time              = $last_boot_time
+    installed_update_count = $installedUpdates.count
+    last_update_install    = "$dateOfLastUpdateInstall" # string needed for proper json
+    updates_last_installed = $updatesLastInstalled
+    last_boot_time         = $last_boot_time
 }
 # write out json
 $update_meta | ConvertTo-Json
