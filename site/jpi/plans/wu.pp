cc7b9397cf2ae39a35c421834e74ca1fadb76719
Joey Piccola <joey.piccola@gmail.com>
2019-11-13 14:27:22 -0700

    fix

--- site/jpi/plans/wu.pp
+++ site/jpi/plans/wu.pp
@@ -2,15 +2,12 @@ plan jpi::wu (
   TargetSpec $nodes
 ) {
   # run task to get updates
-  $wu_report_task_result = run_task('jpi::wu_report', $nodes, update_report => 'missing')
+  $wu_report_task_result = run_task('jpi::wu_report', $nodes, '_catch_errors' => true, update_report => 'missing')
   # nodes that succeeded to run the task
-  $succeeded = $wu_report_task_result.ok_set
-  # ok set dot names
-  $succeeded_names = $succeeded.names
-
-  out::message("succeeded: ${succeeded_names}}")
+  $succeeded = $wu_report_task_result.ok_set |$result| { $result }.names
+  out::message("succeeded: ${succeeded}}")
   # nodes that failed to run the task
-  $failed = $wu_report_task_result.error_set
+  $failed = $wu_report_task_result.error_set |$result| { $result }.names
   out::message("failed: ${failed}}")
 
   # # parse out targets from the previous task that has missing updates
