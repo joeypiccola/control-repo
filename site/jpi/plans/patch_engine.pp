4d8f41c2d01ac31ab6faa92390ce8ac5b3f0e413
Joey Piccola <joey.piccola@gmail.com>
2019-11-13 21:17:25 -0700

    make vars unique

--- site/jpi/plans/patch_engine.pp
+++ site/jpi/plans/patch_engine.pp
@@ -3,43 +3,43 @@ plan jpi::patch_engine (
 ) {
   ## ROUND #1
   # get needed updates
-  $get_updates = run_task('jpi::get_updates', $nodes)
+  $get_updates_1 = run_task('jpi::get_updates', $nodes)
   # get system(s) that have missing updates
-  $targets_with_updates = $get_updates.filter_set |$result| { $result['count'] > 0 }.targets
+  $targets_with_updates_1 = $get_updates_1.filter_set |$result| { $result['count'] > 0 }.targets
   # if there are no systems missing updates then exit
-  if ($targets_with_updates.empty) {
+  if ($targets_with_updates_1.empty) {
     break
   }
   # install updates on nodes
-  run_task('jpi::install_updates', $targets_with_updates)
+  run_task('jpi::install_updates', $targets_with_updates_1)
   # reboot the system
-  run_plan('reboot', $targets_with_updates)
+  run_plan('reboot', $targets_with_updates_1)
 
   ## ROUND #2
   # get needed updates
-  $get_updates = run_task('jpi::get_updates', $nodes)
+  $get_updates_2 = run_task('jpi::get_updates', $nodes)
   # get system(s) that have missing updates
-  $targets_with_updates = $get_updates.filter_set |$result| { $result['count'] > 0 }.targets
+  $targets_with_updates_2 = $get_updates_2.filter_set |$result| { $result['count'] > 0 }.targets
   # if there are no systems missing updates then exit
-  if ($targets_with_updates.empty) {
+  if ($targets_with_updates_2.empty) {
     break
   }
   # install updates on nodes
-  run_task('jpi::install_updates', $targets_with_updates)
+  run_task('jpi::install_updates', $targets_with_updates_2)
   # reboot the system
-  run_plan('reboot', $targets_with_updates)
+  run_plan('reboot', $targets_with_updates_2)
 
   ## ROUND #3
   # get needed updates
-  $get_updates = run_task('jpi::get_updates', $nodes)
+  $get_updates_3 = run_task('jpi::get_updates', $nodes)
   # get system(s) that have missing updates
-  $targets_with_updates = $get_updates.filter_set |$result| { $result['count'] > 0 }.targets
+  $targets_with_updates_3 = $get_updates_3.filter_set |$result| { $result['count'] > 0 }.targets
   # if there are no systems missing updates then exit
-  if ($targets_with_updates.empty) {
+  if ($targets_with_updates_3.empty) {
     break
   }
   # install updates on nodes
-  run_task('jpi::install_updates', $targets_with_updates)
+  run_task('jpi::install_updates', $targets_with_updates_3)
   # reboot the system
-  run_plan('reboot', $targets_with_updates)
+  run_plan('reboot', $targets_with_updates_3)
 }
