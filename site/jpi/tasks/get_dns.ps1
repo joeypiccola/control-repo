86d74442f53bd776c07c3d77c48e8940922ebae8
Joey Piccola <joey.piccola@gmail.com>
2020-02-17 14:46:26 -0700

    use write out

--- site/jpi/tasks/get_dns.ps1
+++ site/jpi/tasks/get_dns.ps1
@@ -7,7 +7,7 @@ $VerbosePreference     = 'Continue'
 $activeNICs = Get-WmiObject -Class 'Win32_NetworkAdapterConfiguration'| Where-Object { ($null -ne $_.IPAddress) -and ($null -ne $_.DNSServerSearchOrder) }
 # write out data on found NICs. JSON where we can :(
 if ($host.Version.Major -gt 2) {
-    $activeNICs | Select-Object -Property Index, IPAddress, DNSServerSearchOrder, MACAddress | ConvertTo-Json
+    Write-Output ($activeNICs | Select-Object -Property Index, IPAddress, DNSServerSearchOrder, MACAddress | ConvertTo-Json)
 } else {
     $activeNICs | Format-Table -Property Index, IPAddress, DNSServerSearchOrder, MACAddress -AutoSize | Out-String -Width 4096
 }
