64f2019fe67aa3f6940cce19ffc4fb9636378a47
Joey Piccola <joey.piccola@gmail.com>
2020-03-30 15:57:54 -0600

    efficiency

--- site/jpi/tasks/set_patch_group.ps1
+++ site/jpi/tasks/set_patch_group.ps1
@@ -6,14 +6,11 @@ Param (
     [string]$patch_group
 )
 
-
 # define pref vars
 $ErrorActionPreference = 'Stop'
 $WarningPreference     = 'Continue'
 $VerbosePreference     = 'Continue'
 
-
-Write-Verbose 'this is a verbose test'
 
 if ($action -eq 'set' -and [string]::IsNullOrEmpty($patch_group)) {
     Write-Error "Action of $action was defined but no patch_group was provided."
@@ -22,32 +19,40 @@ if ($action -eq 'set' -and [string]::IsNullOrEmpty($patch_group)) {
 $fact_file_path = "C:\ProgramData\PuppetLabs\facter\facts.d\patch_group.txt"
 $fact_file_existence = Test-Path -Path $fact_file_path
 
-switch ($action) {
-    'get' {
-        # get the value of the provided fact.
-        if ($fact_file_existence) {
-            $patch_group = (Get-Content -Path $fact_file_path).Split('=')[1]
+Write-Verbose "Running with action: $action."
+Write-Verbose "patch_group fact path: $fact_file_path."
+Write-Verbose "path_group fact existance: $fact_file_existence."
+
+function Get-PatchGroupFact {
+    # get the value of the patch group fact
+    if ($fact_file_existence) {
+        $patch_group = (Get-Content -Path $fact_file_path).Split('=')[1]
+        Write-Verbose "patch_group fact value: $patch_group."
+        if ($action -eq 'get') {
             Write-Output $patch_group
-        } else {
-            Write-Verbose "$fact_file_path not found to get."
-        }
-    }
-    'set' {
-        # set the provided fact. if it does not exist create it
-        if ($fact_file_existence) {
-            Remove-Item -Path $fact_file_path -Force
-        }
-        New-Item -ItemType File -Path $fact_file_path -Value "patch_group=$patch_group" | out-null
-    }
-    'delete' {
-        # delete the provided fact.
-        if ($fact_file_existence) {
-            Remove-Item -Path $fact_file_path -Force
-        } else {
-            Write-Verbose "$fact_file_path not found to delete."
         }
     }
 }
 
+Get-PatchGroupFact
 
-
+switch ($action) {
+    'get' {
+        # do nothing because technically we already wrote it out in verbose
+    }
+    'set' {
+        if ($fact_file_existence) {
+            Write-Verbose "Removing $fact_file_path."
+            Remove-Item -Path $fact_file_path -Force
+        }
+        $fact_contents = "patch_group=$patch_group"
+        Write-Verbose "Creating $fact_file_path with `"$fact_contents`"."
+        New-Item -ItemType File -Path $fact_file_path -Value $fact_contents | Out-Null
+    }
+    'delete' {
+        if ($fact_file_existence) {
+            Write-Verbose "Removing $fact_file_path."
+            Remove-Item -Path $fact_file_path -Force
+        }
+    }
+}
