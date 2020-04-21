adaeae20243daacb75a543c1c4da6b11b4d12479
Joey Piccola <joey.piccola@gmail.com>
2020-04-20 18:40:03 -0600

    cleaner logic

--- site/jpi/tasks/testconnection.ps1
+++ site/jpi/tasks/testconnection.ps1
@@ -1,4 +1,3 @@
-[CmdletBinding()]
 Param (
     [Parameter()]
     [string]$hostname,
@@ -12,12 +11,7 @@ try {
     (New-Object Net.Sockets.TcpClient).Connect($hostname, $port)
     $test = $true
 } catch {
-    $test = $false
-    $errorVar = $_.Exception.Message
+    Write-Error "Unable to connect to $hostname on tcp\$port. Error: $($_.Exception.Message)."
 }
 
-if (($erroronfail) -and ($test -eq $false)) {
-    Write-Error "Unable to connect to $hostname on tcp\$port. Error: $errorVar."
-} else {
-    Write-Output $test
-}
+Write-Output $test
