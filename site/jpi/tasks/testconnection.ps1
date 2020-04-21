36d43ea15f3a06dc4b821b2e521f51da8c2d09e3
Joey Piccola <joey.piccola@gmail.com>
2020-04-20 21:03:32 -0600

    use json on the output

--- site/jpi/tasks/testconnection.ps1
+++ site/jpi/tasks/testconnection.ps1
@@ -4,14 +4,10 @@ Param (
     [int]$port,
     [switch]$erroronfail
 )
-
 $ErrorActionPreference = 'stop'
-
 try {
     (New-Object Net.Sockets.TcpClient).Connect($hostname, $port)
-    $test = $true
 } catch {
     Write-Error "Unable to connect to $hostname on tcp\$port. Error: $($_.Exception.Message)."
 }
-
-Write-Output $test
+Write-Output "{`"message`": `"Successfully connected $hostname on tcp\$port.`"}"
