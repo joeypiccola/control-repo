c86f35604249745dfd06620bcac6a9ba5847e040
Joey Piccola <joey.piccola@gmail.com>
2020-04-24 10:25:34 -0600

    service stats

---
+++ site/jpi/tasks/service_stats.ps1
@@ -0,0 +1,48 @@
+Param (
+    [Parameter(Mandatory = $True)]
+    [string]$servicename
+)
+
+# https://dkalemis.wordpress.com/2015/03/02/visualize-the-services-graph-of-your-windows-os/
+
+function List-DependentServices ($inputService, $inputPadding) {
+    if ($inputService.DependentServices) {
+        $padding = "   " + $inputPadding
+
+        $output = $padding + "The following services depend on " + $inputService.DisplayName
+        $output
+
+        $padding = "   " + $padding
+
+        foreach ($ds in $inputService.DependentServices) {
+            $output = $padding + $ds.DisplayName
+            $output
+
+            List-DependentServices $ds $padding
+        }
+    }
+}
+
+function List-ServicesDependedOn ($inputService, $inputPadding) {
+    if ($inputService.ServicesDependedOn) {
+        $padding = "   " + $inputPadding
+
+        $output = $padding + "The following services are required by " + $inputService.DisplayName
+        $output
+
+        $padding = "   " + $padding
+
+        foreach ($rs in $inputService.ServicesDependedOn) {
+            $output = $padding + $rs.DisplayName
+            $output
+
+            List-ServicesDependedOn $rs $padding
+        }
+    }
+}
+
+$service = Get-Service -Name $servicename -erroraction stop
+$output = $service.DisplayName
+$output
+List-DependentServices $service ""
+List-ServicesDependedOn $service ""
