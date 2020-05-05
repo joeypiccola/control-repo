11d9cac9bfdf9609e669ac44e15e73c3c39a3016
Joey Piccola <joey.piccola@gmail.com>
2020-05-05 10:03:34 -0600

    use dsc_psdscrunascredential

--- site/profile/manifests/cluster/test.pp
+++ site/profile/manifests/cluster/test.pp
@@ -15,11 +15,11 @@ class profile::cluster::test (
   }
 
   dsc_xadcomputer {'computer_test':
-    dsc_computername      => 'mySweetComputer',
-    #dsc_enabledoncreation => true,
-    dsc_path              => 'OU=Staging,OU=LabStuff,OU=Servers,DC=ad,DC=piccola,DC=us',
-    dsc_domaincontroller  => 'dc04.ad.piccola.us',
-    dsc_credential        => {
+    dsc_computername         => 'mySweetComputer',
+    #dsc_enabledoncreation   => true,
+    dsc_path                 => 'OU=Staging,OU=LabStuff,OU=Servers,DC=ad,DC=piccola,DC=us',
+    dsc_domaincontroller     => 'dc04.ad.piccola.us',
+    dsc_psdscrunascredential => {
       'user'     => $dsc_psdscrunascredential_user,
       'password' => Sensitive($dsc_psdscrunascredential_password)
     },
