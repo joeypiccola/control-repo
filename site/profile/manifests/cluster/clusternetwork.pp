7e345ad93f6f54cce8c378bdb07505386d26a215
Joey Piccola <joey.piccola@gmail.com>
2020-05-05 23:37:01 -0600

    add back

--- site/profile/manifests/cluster/clusternetwork.pp
+++ site/profile/manifests/cluster/clusternetwork.pp
@@ -2,15 +2,15 @@
 class profile::cluster::clusternetwork (
   String $dsc_address,
   String $dsc_addressmask,
-  #String $dsc_name,
-  #String $dsc_role,
+  String $dsc_name,
+  String $dsc_role,
 ) {
 
   dsc_xclusternetwork {'properties':
     dsc_address     => $dsc_address,
     dsc_addressmask => $dsc_addressmask,
-    #dsc_name        => $dsc_name,
-    #dsc_role        => $dsc_role,
+    dsc_name        => $dsc_name,
+    dsc_role        => $dsc_role,
   }
 
 }
