06b03122843813640dfe8a113f5c873e32b7e453
Joey Piccola <joey.piccola@gmail.com>
2020-05-05 21:25:03 -0600

    cluster go go

--- site/profile/manifests/cluster.pp
+++ site/profile/manifests/cluster.pp
@@ -1,7 +1,7 @@
 # == Class: profile::cluster
 class profile::cluster {
-  include profile::cluster::test
-  #include profile::cluster::cluster
+  #include profile::cluster::test
+  include profile::cluster::cluster
   #include profile::cluster::clusterdisk
   #include profile::cluster::clusternetwork
   #include profile::cluster::clusterprefferedowner
---
+++ site/profile/manifests/cluster/cluster.pp
@@ -0,0 +1,33 @@
+# == Class: profile::cluster::cluster
+class profile::cluster::cluster (
+  String  $dsc_domainadministratorcredential_password,
+  String  $dsc_domainadministratorcredential_user,
+  String  $dsc_master,
+  String  $dsc_name,
+  Integer $dsc_retrycount,
+  Integer $dsc_retryintervalsec,
+  String  $dsc_staticipaddress,
+) {
+
+  if $facts['hostname'] == $dsc_master {
+    dsc_xcluster {'create':
+      dsc_domainadministratorcredential => {
+        'user'     => $dsc_domainadministratorcredential_user,
+        'password' => Sensitive($dsc_domainadministratorcredential_password)
+      },
+      dsc_name                          => $dsc_name,
+      dsc_staticipaddress               => $dsc_staticipaddress,
+    }
+  } else {
+    dsc_xwaitforcluster {'wait':
+      dsc_name             => $dsc_name,
+      dsc_retrycount       => $dsc_retrycount,
+      dsc_retryintervalsec => $dsc_retryintervalsec,
+    }
+    dsc_xcluster {'add':
+      dsc_name  => $dsc_name,
+      requrires => Dsc_xWaitForCluster['wait'],
+    }
+  }
+
+}
