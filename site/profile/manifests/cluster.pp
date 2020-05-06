ab69e30cf471bb0b4c383eeee0161e4dacd36c94
Joey Piccola <joey.piccola@gmail.com>
2020-05-05 22:11:45 -0600

    cluster network init

--- site/profile/manifests/cluster.pp
+++ site/profile/manifests/cluster.pp
@@ -3,7 +3,7 @@ class profile::cluster {
   #include profile::cluster::test
   include profile::cluster::cluster
   #include profile::cluster::clusterdisk
-  #include profile::cluster::clusternetwork
+  include profile::cluster::clusternetwork
   #include profile::cluster::clusterprefferedowner
   #include profile::cluster::clusterproperty
   #include profile::cluster::clusterquorum
---
+++ site/profile/manifests/cluster/clusernetwork.pp
@@ -0,0 +1,16 @@
+# == Class: profile::cluster::clusternetwork
+class profile::cluster::clusternetwork (
+  String  $dsc_address,
+  String  $dsc_addressmask,
+  String  $dsc_name,
+  Integer $dsc_role,
+) {
+
+  dsc_xclusternetwork {'properties':
+    dsc_address     => $dsc_address,
+    dsc_addressmask => $dsc_addressmask,
+    dsc_name        => $dsc_name,
+    dsc_role        => $dsc_role,
+  }
+
+}
