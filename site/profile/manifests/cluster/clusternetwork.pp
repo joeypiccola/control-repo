3fd967e2796911498494bb30764b4beba42dc2e7
Joey Piccola <joey.piccola@gmail.com>
2020-05-05 23:31:50 -0600

    test

--- site/profile/manifests/cluster/clusternetwork.pp
+++ site/profile/manifests/cluster/clusternetwork.pp
@@ -3,14 +3,14 @@ class profile::cluster::clusternetwork (
   String $dsc_address,
   String $dsc_addressmask,
   #String $dsc_name,
-  String $dsc_role,
+  #String $dsc_role,
 ) {
 
   dsc_xclusternetwork {'properties':
     dsc_address     => $dsc_address,
     dsc_addressmask => $dsc_addressmask,
     #dsc_name        => $dsc_name,
-    dsc_role        => $dsc_role,
+    #dsc_role        => $dsc_role,
   }
 
 }
