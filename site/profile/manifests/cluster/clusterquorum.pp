444604ddb960bbbb2c92a8d0d7980370ab59cb7b
Joey Piccola <joey.piccola@gmail.com>
2020-05-06 14:54:46 -0600

    fix types

--- site/profile/manifests/cluster/clusterquorum.pp
+++ site/profile/manifests/cluster/clusterquorum.pp
@@ -15,7 +15,7 @@ class profile::cluster::clusterquorum (
 ) {
 
   if $facts['hostname'] == $dsc_master {
-    # online, init, format, letter and lavel the quorum drive
+    # online, init, format, letter and label the quorum drive
     dsc_disk {'quorum_disk':
       dsc_allocationunitsize => $dsc_allocationunitsize,
       dsc_driveletter        => $dsc_driveletter,
@@ -31,18 +31,18 @@ class profile::cluster::clusterquorum (
       dsc_retrycount       => $dsc_retrycount,
     }
 
-    # dsc_xclusterdisk {'quorum_cluster_disk':
-    #   dsc_number => $dsc_number,
-    #   dsc_label  => $dsc_drivelabel,
-    #   require    => Dsc_waitforvolume['quorum_disk_wait']
-    # }
+    dsc_xclusterdisk {'quorum_cluster_disk':
+      dsc_number => $dsc_number,
+      dsc_label  => $dsc_drivelabel,
+      require    => Dsc_waitforvolume['quorum_disk_wait']
+    }
 
-    # dsc_xclusterquorum {'SetQuorumToDiskOnly':
-    #   issingleinstance => $dsc_issingleinstance,
-    #   type             => $dsc_type,
-    #   resource         => $dsc_drivelabel,
-    #   require          => Dsc_xclusterdisk['quorum_cluster_disk']
-    # }
+    dsc_xclusterquorum {'SetQuorumToDiskOnly':
+      issingleinstance => $dsc_issingleinstance,
+      type             => $dsc_type,
+      resource         => $dsc_drivelabel,
+      require          => Dsc_xclusterdisk['quorum_cluster_disk']
+    }
   }
 
 }
