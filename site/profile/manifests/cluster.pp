a966173e59746229a3c9b3842028028b89d95130
Joey Piccola <joey.piccola@gmail.com>
2020-05-06 14:51:01 -0600

    cluster quorum init

--- site/profile/manifests/cluster.pp
+++ site/profile/manifests/cluster.pp
@@ -1,15 +1,14 @@
 # == Class: profile::cluster
 class profile::cluster {
-  #include profile::cluster::test
   include profile::cluster::cluster
-  #include profile::cluster::clusterdisk
   include profile::cluster::clusternetwork
   #include profile::cluster::clusterprefferedowner
   #include profile::cluster::clusterproperty
-  #include profile::cluster::clusterquorum
+  include profile::cluster::clusterquorum
   include profile::cluster::services
 
   Class['profile::cluster::services']
   -> Class['profile::cluster::cluster']
   -> Class['profile::cluster::clusternetwork']
+  -> Class['profile::cluster::clusterquorum']
 }
---
+++ site/profile/manifests/cluster/clusterquorum.pp
@@ -0,0 +1,48 @@
+# == Class: profile::cluster::clusterquorum
+class profile::cluster::clusterquorum (
+  String $dsc_allocationunitsize,
+  String $dsc_driveletter,
+  String $dsc_drivelabel,
+  String $dsc_partitionstyle,
+  String $dsc_diskidtype,
+  String $dsc_diskid,
+  Integer $dsc_retryintervalsec,
+  Integer $dsc_retrycount,
+  String $dsc_issingleinstance,
+  String $dsc_type,
+  String $dsc_number,
+  String $dsc_master
+) {
+
+  if $facts['hostname'] == $dsc_master {
+    # online, init, format, letter and lavel the quorum drive
+    dsc_disk {'quorum_disk':
+      dsc_allocationunitsize => $dsc_allocationunitsize,
+      dsc_driveletter        => $dsc_driveletter,
+      dsc_drivelabel         => $dsc_drivelabel,
+      dsc_partitionstyle     => $dsc_partitionstyle,
+      dsc_diskidtype         => $dsc_diskidtype,
+      dsc_diskid             => $dsc_diskid,
+    }
+
+    dsc_waitforvolume {'quorum_disk_wait':
+      dsc_driveletter      => $dsc_driveletter,
+      dsc_retryintervalsec => $dsc_retryintervalsec,
+      dsc_retrycount       => $dsc_retrycount,
+    }
+
+    # dsc_xclusterdisk {'quorum_cluster_disk':
+    #   dsc_number => $dsc_number,
+    #   dsc_label  => $dsc_drivelabel,
+    #   require    => Dsc_waitforvolume['quorum_disk_wait']
+    # }
+
+    # dsc_xclusterquorum {'SetQuorumToDiskOnly':
+    #   issingleinstance => $dsc_issingleinstance,
+    #   type             => $dsc_type,
+    #   resource         => $dsc_drivelabel,
+    #   require          => Dsc_xclusterdisk['quorum_cluster_disk']
+    # }
+  }
+
+}
