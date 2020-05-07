cb5226a546339a97aa50559f001c07e977a29d75
Joey Piccola <joey.piccola@gmail.com>
2020-05-07 10:26:37 -0600

    ugh

--- site/profile/manifests/cluster/clusterquorum.pp
+++ site/profile/manifests/cluster/clusterquorum.pp
@@ -33,8 +33,9 @@ class profile::cluster::clusterquorum (
     exec {'quorum_cluster_disk_add':
       provider => 'powershell',
       command  => "Import-Module FailoverClusters
-                   Get-Disk -UniqueId ${dsc_diskid} | Add-ClusterDisk",
+                   Get-Disk -UniqueId '${dsc_diskid}' | Add-ClusterDisk",
       onlyif   => "$diskInstance = Get-CimInstance -ClassName MSCluster_Disk -Namespace \'Root\\MSCluster\' | Where-Object {$_.UniqueId -eq '${dsc_diskid}'}
+                   $diskInstance | ConvertTo-Json -Depth 1 | out-file c:\\di.txt
                    if ($null -ne $diskInstance) {
                      exit 1
                    }",
