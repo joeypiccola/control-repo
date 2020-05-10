ee47b84e550897a930c37406f7cdc415e9e3a698
Joey Piccola <joey.piccola@gmail.com>
2020-05-09 19:09:14 -0600

    pass creds

--- site/profile/manifests/cluster/clusterproperty.pp
+++ site/profile/manifests/cluster/clusterproperty.pp
@@ -5,14 +5,20 @@ class profile::cluster::clusterproperty (
   String $dsc_clusterlogsize,
   String $dsc_description,
   String $dsc_master,
+  String $user,
+  String $pass,
 ) {
 
   if $facts['hostname'] == $dsc_master {
     dsc_xclusterproperty {'set_cluster_properties':
-      dsc_name            => $dsc_name,
-      dsc_clusterloglevel => $dsc_clusterloglevel,
-      dsc_clusterlogsize  => $dsc_clusterlogsize,
-      dsc_description     => $dsc_description,
+      dsc_name                 => $dsc_name,
+      dsc_clusterloglevel      => $dsc_clusterloglevel,
+      dsc_clusterlogsize       => $dsc_clusterlogsize,
+      dsc_description          => $dsc_description,
+      dsc_psdscrunascredential => {
+        'user'     => $user,
+        'password' => Sensitive($pass)
+      },
     }
   }
 
