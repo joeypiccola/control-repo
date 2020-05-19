cc207ce85de05309876e9c38d232ccbcec61a6ea
Joey Piccola <joey.piccola@gmail.com>
2020-05-19 14:47:39 -0600

    name

--- site/profile/manifests/wsus/server.pp
+++ site/profile/manifests/wsus/server.pp
@@ -15,14 +15,14 @@ class profile::wsus::server (
 
   dsc_xwebapppool { 'WsusPool':
     dsc_ensure                    => 'present',
-    dsc_identitytype              => 'NetworkService',
-    dsc_idletimeout               => '0:00:00',
-    dsc_managedpipelinemode       => 'Integrated',
+    #dsc_identitytype              => 'NetworkService',
+    #dsc_idletimeout               => '0:00:00',
+    #dsc_managedpipelinemode       => 'Integrated',
     dsc_name                      => 'WsusPool',
-    dsc_pingingenabled            => true,
-    dsc_queuelength               => 2000,
-    dsc_restartprivatememorylimit => 0,
-    dsc_state                     => 'Started',
+    #dsc_pingingenabled            => true,
+    #dsc_queuelength               => 2000,
+    #dsc_restartprivatememorylimit => 0,
+    #dsc_state                     => 'Started',
     #require                       => Exec['WsusUtil PostInstall'],
   }
 
