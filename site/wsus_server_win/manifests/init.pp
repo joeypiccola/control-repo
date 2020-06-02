9d1a849579a523abe8a35d1dc318943225d1e91f
Joey Piccola <joey.piccola@gmail.com>
2020-06-02 10:35:40 -0600

    fix params

--- site/wsus_server_win/manifests/init.pp
+++ site/wsus_server_win/manifests/init.pp
@@ -1,8 +1,8 @@
 # == Class: wsus_server_win
 class wsus_server_win (
-  Array  $wsus_server_win::params::iis_features,
-  Array  $wsus_server_win::params::wsus_features,
-  String $wsus_server_win::params::wsus_directory,
+  Array $iis_features = $wsus_server_win::params::iis_features,
+  Array $wsus_features = $wsus_server_win::params::wsus_features,
+  String $wsus_directory = $wsus_server_win::params::wsus_directory,
 ) inherits wsus_server_win::params {
 
   include wsus_server_win::iis
