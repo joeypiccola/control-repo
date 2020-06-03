169cfd82ba6ae0d69051eb6c6d9c60b394b0c40e
Joey Piccola <joey.piccola@gmail.com>
2020-06-03 15:40:01 -0600

    fix type and re-sort

--- site/wsus_server_win/manifests/init.pp
+++ site/wsus_server_win/manifests/init.pp
@@ -2,9 +2,9 @@
 class wsus_server_win (
   Array $iis_features,
   Array $wsus_features,
+  Boolean $iis_wsus_app_pool_pinging_enabled,
   String $iis_wsus_app_pool_identity_type,
   String $iis_wsus_app_pool_idle_timeout,
-  String $iis_wsus_app_pool_pinging_enabled,
   String $iis_wsus_app_pool_private_memory,
   String $iis_wsus_app_pool_queue_length,
   String $iis_wsus_app_pool_restart_time_limit,
