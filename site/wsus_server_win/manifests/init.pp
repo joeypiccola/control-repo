18b467fc021b767a5886cfd0774beb90d6283668
Joey Piccola <joey.piccola@gmail.com>
2020-06-03 15:35:47 -0600

    fix name on iis params and sort includes

--- site/wsus_server_win/manifests/init.pp
+++ site/wsus_server_win/manifests/init.pp
@@ -2,17 +2,17 @@
 class wsus_server_win (
   Array $iis_features,
   Array $wsus_features,
-  String $iis_identity_type,
-  String $iis_idle_timeout,
-  String $iis_pinging_enabled,
-  String $iis_private_memory,
-  String $iis_queue_length,
-  String $iis_restart_time_limit,
+  String $iis_wsus_app_pool_identity_type,
+  String $iis_wsus_app_pool_idle_timeout,
+  String $iis_wsus_app_pool_pinging_enabled,
+  String $iis_wsus_app_pool_private_memory,
+  String $iis_wsus_app_pool_queue_length,
+  String $iis_wsus_app_pool_restart_time_limit,
   String $wsus_directory,
 ) {
 
-  include wsus_server_win::iis::install
   include wsus_server_win::iis::config
+  include wsus_server_win::iis::install
   include wsus_server_win::wsus::install
 
   Class['wsus_server_win::iis::install']
