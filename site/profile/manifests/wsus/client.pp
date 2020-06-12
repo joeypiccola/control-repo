8408e2bd30ca2958e65290809488932f8ee449b3
Joey Piccola <joey.piccola@gmail.com>
2020-06-12 12:31:53 -0600

    shutdown options

--- site/profile/manifests/wsus/client.pp
+++ site/profile/manifests/wsus/client.pp
@@ -9,6 +9,18 @@ class profile::wsus::client (
         notify => Exec['wuauclt'] ,
       }
 
+    registry_value { 'HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate\AU\NoAUShutdownOption':
+      type => dword,
+      data => 1,
+      #require => Registry_key['HKLM\SYSTEM\CurrentControlSet\services\mrxsmb10']
+    }
+
+    registry_value { 'HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate\AU\NoAUAsDefaultShutdownOption':
+      type => dword,
+      data => 1,
+      #require => Registry_key['HKLM\SYSTEM\CurrentControlSet\services\mrxsmb10']
+    }
+
       # If running a kernelmajversion >= 10.0 then use usoclient else use wuauclt
       if versioncmp($facts[kernelmajversion], '10.0') >= 0 {
         $cmd = 'usoclient startscan'
