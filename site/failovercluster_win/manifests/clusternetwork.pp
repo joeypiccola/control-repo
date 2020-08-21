3c633daeb8f8ff10f81fce2d32506b59dc21ea63
Joey Piccola <joey.piccola@gmail.com>
2020-08-21 09:48:55 -0600

    fix quote string

--- site/failovercluster_win/manifests/clusternetwork.pp
+++ site/failovercluster_win/manifests/clusternetwork.pp
@@ -8,7 +8,7 @@ class failovercluster_win::clusternetwork {
     dsc_role        => $failovercluster_win::client_network_role,
   }
 
-  if $failovercluster_win::network_strategy == separate_cluster_client_network {
+  if $failovercluster_win::network_strategy == 'separate_cluster_client_network' {
     dsc_xclusternetwork {'cluster_network':
       dsc_address     => $failovercluster_win::cluster_network_address,
       dsc_addressmask => $failovercluster_win::cluster_network_address_mask,
