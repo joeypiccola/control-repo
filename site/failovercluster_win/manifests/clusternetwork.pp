d66a94d6141f3f2b5a170b5532e739ff17df0955
Joey Piccola <joey.piccola@gmail.com>
2020-08-21 09:01:06 -0600

    network refactor

--- site/failovercluster_win/manifests/clusternetwork.pp
+++ site/failovercluster_win/manifests/clusternetwork.pp
@@ -8,11 +8,13 @@ class failovercluster_win::clusternetwork {
     dsc_role        => $failovercluster_win::client_network_role,
   }
 
-  #dsc_xclusternetwork {'cluster_network':
-  #  dsc_address     => $dsc_cluster_address,
-  #  dsc_addressmask => $dsc_cluster_addressmask,
-  #  dsc_name        => $dsc_cluster_name,
-  #  dsc_role        => $dsc_cluster_role,
-  #}
+  if $failovercluster_win::network_design == separate_cluster_client_network {
+    dsc_xclusternetwork {'cluster_network':
+      dsc_address     => $failovercluster_win::cluster_network_address,
+      dsc_addressmask => $failovercluster_win::cluster_network_address_mask,
+      dsc_name        => $failovercluster_win::cluster_network_name,
+      dsc_role        => $failovercluster_win::cluster_network_role,
+    }
+  }
 
 }
--- site/failovercluster_win/manifests/init.pp
+++ site/failovercluster_win/manifests/init.pp
@@ -4,20 +4,21 @@ class failovercluster_win (
   String $ad_user,
   String $client_network_address_mask,
   String $client_network_address,
-  String $client_network_name,
-  String $client_network_role,
   String $cluster_ip,
   String $cluster_name,
   String $primary_node,
   String $quorum_diskid,
   Array $service_names = ['Failover-Clustering'],
   Boolean $manage_quorum = true,
+  Enum['separate_cluster_client_network','single_cluster_client_network'] $network_strategy = 'single_cluster_client_network',
   Integer $ancillary_node_retry_count = 60,
   Integer $ancillary_node_retry_interval_sec = 60,
   Integer $quorum_retry_count = 10,
   Integer $quorum_retry_interval_sec = 30,
-  Optional[String] $local_admin_identity = undef,
-  Optional[Boolean] $manage_local_admin = undef,
+  String $client_network_name = 'Client Network',
+  String $client_network_role = '0',
+  String $cluster_network_name = 'Cluster Network',
+  String $cluster_network_role = '1',
   String $description = 'Microsoft Windows Failover Cluster',
   String $log_level = '3',
   String $log_size = '1024',
@@ -28,10 +29,21 @@ class failovercluster_win (
   String $quorum_is_single_instance = 'Yes',
   String $quorum_partition_style = 'GPT',
   String $quorum_type = 'DiskOnly',
+  Optional[Boolean] $manage_local_admin = undef,
+  Optional[String] $cluster_network_address = undef,
+  Optional[String] $cluster_network_address_mask = undef,
+  Optional[String] $local_admin_identity = undef,
 ) {
 
+  # if manage_local_admin is true then ensure a local admin has been defined
   if $manage_local_admin {
     assert_type(Array[NotUndef], [$local_admin_identity])
+  }
+
+  # if network strategy indicates two seperate networks ensure optional cluster network params are defined
+  if $network_strategy == 'separate_cluster_client_network' {
+    assert_type(Array[NotUndef], [$cluster_network_address_mask])
+    assert_type(Array[NotUndef], [$cluster_network_address])
   }
 
   include failovercluster_win::cluster
