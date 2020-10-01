8b39bcf2d1d05e84aa5de0d26fdbe18f9ab652be
Joey Piccola <joey.piccola@gmail.com>
2020-09-30 23:09:20 -0600

    rm stage dir

--- site/profile/manifests/sql.pp
+++ site/profile/manifests/sql.pp
@@ -8,10 +8,10 @@ class profile::sql {
   $soruce_iso_file = basename($source_iso_url) #SQLServer2019-x64-ENU-Dev.iso
   $source_iso_file_path = "${source_iso_download_dir}/${soruce_iso_file}" # d:/install_media/SQLServer2019-x64-ENU-Dev.iso
 
-  file { 'create_source_iso_download_dir':
-    ensure => 'directory',
-    path   => $source_iso_download_dir,
-  }
+  #file { 'create_source_iso_download_dir':
+  #  ensure => 'directory',
+  #  path   => $source_iso_download_dir,
+  #}
 
   file { 'download_source_iso':
     ensure  => 'present',
