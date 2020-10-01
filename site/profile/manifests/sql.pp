7adad0e4f24849fa57823b18f3bb0ce7e7824bff
Joey Piccola <joey.piccola@gmail.com>
2020-10-01 08:37:07 -0600

    add instance bits

--- site/profile/manifests/sql.pp
+++ site/profile/manifests/sql.pp
@@ -5,7 +5,7 @@ class profile::sql {
   $source_iso_url = 'http://nuget.ad.piccola.us:8081/SQLServer2019-x64-ENU-Dev.iso'
   $source_iso_download_dir = 'F:/install_media'
   $source_iso_mount_drive_letter = 'G'
-  $soruce_iso_file = basename($source_iso_url) #SQLServer2019-x64-ENU-Dev.iso
+  $soruce_iso_file = basename($source_iso_url) # SQLServer2019-x64-ENU-Dev.iso
   $source_iso_file_path = "${source_iso_download_dir}/${soruce_iso_file}" # d:/install_media/SQLServer2019-x64-ENU-Dev.iso
 
   file { 'create_source_iso_download_dir':
@@ -26,22 +26,22 @@ class profile::sql {
     require      => File['download_source_iso'],
   }
 
-  #sqlserver_instance { 'install_instance':
-  #  name                  => $instance_name,
-  #  source                => $source_iso_mount_drive_letter,
-  #  features              => ['SQLEngine'],
-  #  sql_sysadmin_accounts => [$facts['id']],
-  #  install_switches      => {
-  #    'TCPENABLED'          => 1,
-  #    'SQLBACKUPDIR'        => 'C:\\MSSQLSERVER\\backupdir',
-  #    'SQLTEMPDBDIR'        => 'C:\\MSSQLSERVER\\tempdbdir',
-  #    'INSTALLSQLDATADIR'   => 'C:\\MSSQLSERVER\\datadir',
-  #    'INSTANCEDIR'         => 'C:\\Program Files\\Microsoft SQL Server',
-  #    'INSTALLSHAREDDIR'    => 'C:\\Program Files\\Microsoft SQL Server',
-  #    'INSTALLSHAREDWOWDIR' => 'C:\\Program Files (x86)\\Microsoft SQL Server'
-  #  },
-  #  require               => Mount_iso[mount_source_iso],
-  #}
+  sqlserver_instance { 'install_instance':
+    name                  => $instance_name,
+    source                => $source_iso_mount_drive_letter,
+    features              => ['SQL'],
+    sql_sysadmin_accounts => [$facts['id']],
+    install_switches      => {
+      'TCPENABLED'          => 1,
+      'SQLBACKUPDIR'        => 'C:\\MSSQLSERVER\\backupdir',
+      'SQLTEMPDBDIR'        => 'C:\\MSSQLSERVER\\tempdbdir',
+      'INSTALLSQLDATADIR'   => 'C:\\MSSQLSERVER\\datadir',
+      'INSTANCEDIR'         => 'C:\\Program Files\\Microsoft SQL Server',
+      'INSTALLSHAREDDIR'    => 'C:\\Program Files\\Microsoft SQL Server',
+      'INSTALLSHAREDWOWDIR' => 'C:\\Program Files (x86)\\Microsoft SQL Server'
+    },
+    require               => Mount_iso[mount_source_iso],
+  }
 
 
 }
