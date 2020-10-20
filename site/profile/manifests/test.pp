1ceb58572a52bead2b6e856fd7a99b16010dc5e9
Joey Piccola <joey.piccola@gmail.com>
2020-10-20 10:52:25 -0600

    fix

--- site/profile/manifests/test.pp
+++ site/profile/manifests/test.pp
@@ -1,11 +1,18 @@
 # == Class: profile::test
 class profile::test (
-  $dir_name,
+  $dir_names,
 ) {
 
-  file { 'create_a_dir_from_hiera':
-    ensure => 'directory',
-    path   => "c:/${dir_name}",
+  #file { 'create_a_dir_from_hiera':
+  #  ensure => 'directory',
+  #  path   => "c:/${dir_names}",
+  #}
+
+  $dir_names.each |String $dir_name| {
+    file {'create_a_dir_from_hiera':
+      ensure => 'directory',
+      target => "c:/${dir_name}",
+    }
   }
 
 }
