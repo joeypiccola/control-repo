8ffd7d23609c81854b8a6d474c5c30d1f88adadd
jpiccola <joey.piccola@gmail.com>
2020-10-27 20:56:36 -0600

    just json

--- site/jpi/tasks/streams.ps1
+++ site/jpi/tasks/streams.ps1
@@ -1,8 +1,8 @@
 $VerbosePreference     = 'Continue'
 $InformationPreference = 'Continue'
 
-Write-Verbose "this is verbose"
-Write-Host "this is host"
-Write-Information "this is information"
+#Write-Verbose "this is verbose"
+#Write-Host "this is host"
+#Write-Information "this is information"
 
 @{key='value'} | ConvertTo-Json
