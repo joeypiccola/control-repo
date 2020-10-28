05b897dbb0df59dad7436c0cfbf49fbf4ae9369c
jpiccola <joey.piccola@gmail.com>
2020-10-27 19:16:38 -0600

    streams task

---
+++ site/jpi/tasks/streams.ps1
@@ -0,0 +1,6 @@
+$VerbosePreference     = 'SilentlyContiune'
+$InformationPreference = 'SilentlyContiune'
+
+Write-Verbose "this is verbose"
+Write-Host "this is host"
+Write-Information "this is information"
