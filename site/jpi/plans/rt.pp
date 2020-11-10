b98c486cd765ce6662fca41d42b72f799497b4a0
jpiccola <joey.piccola@gmail.com>
2020-11-10 09:03:46 -0700

    add volume modify

--- site/jpi/plans/rt.pp
+++ site/jpi/plans/rt.pp
@@ -2,8 +2,12 @@ plan jpi::rt (
   TargetSpec $nodes,
   String $serial,
   String $label,
+  String $new_label,
 ) {
     run_task('jpi::disk_set_status', $nodes, '_catch_errors' => true, status => 'online', serial => $serial)
-    #run_task('jpi::volume_set_label', $nodes, '_catch_errors' => true, new_label => $new_label, serial => $serial)
+    run_task('jpi::disk_new_guid', $nodes, '_catch_errors' => true, serial => $serial)
+    run_task('jpi::volume_set_label', $nodes, '_catch_errors' => true, new_label => $new_label, label => $label)
     run_task('jpi::cluster_add_disk', $nodes, '_catch_errors' => true, name => $label, serial => $serial)
+    #run_task('jpi::cluster_add_csv', $nodes, '_catch_errors' => true, disk_resouce_name => $new_label)
+
 }
