e169b7e78d85d3efd84df90efaba9cb5495e1945
jpiccola <joey.piccola@gmail.com>
2020-11-10 22:41:40 -0700

    fix name issue

--- site/jpi/tasks/volume_set_label.ps1
+++ site/jpi/tasks/volume_set_label.ps1
@@ -1,34 +1,34 @@
 [CmdletBinding()]
 Param(
     [Parameter(Mandatory)]
-    [string]$label,
+    [string]$label_key,
     [Parameter(Mandatory)]
-    [string]$new_label
+    [string]$label_new
 )
 
 $ErrorActionPreference = 'stop'
 
-$volume = Get-Volume | Where-Object { $_.FileSystemLabel -eq $label }
-$proposedVolumeLabel = Get-Volume | Where-Object { $_.FileSystemLabel -eq $new_label }
+$volume = Get-Volume | Where-Object { $_.FileSystemLabel -eq $label_key }
+$proposedVolumeLabel = Get-Volume | Where-Object { $_.FileSystemLabel -eq $label_new }
 
 if (($proposedVolumeLabel | Measure-Object).count -gt 0) {
-    Write-Error "Volume with proposed new label already exists: $new_label"
+    Write-Error "Volume with proposed new label already exists: $label_new"
 }
 
 switch (($volume | Measure-Object).count)
 {
     0 {
-        Write-Error "No volume found for provided label: $label"
+        Write-Error "No volume found for provided label: $label_key"
     }
     1 {
-        $volume | Set-Volume -NewFileSystemLabel $new_label
+        $volume | Set-Volume -NewFileSystemLabel $label_new
     }
     {$_ -gt 1} {
-        Write-Error "More than one volume found for provided label: $label"
+        Write-Error "More than one volume found for provided label: $label_key"
     }
 }
 
 @{
-    newLabel = $new_label
-    oldLabel = $volume.FileSystemLabel
+    newLabel = $label_new
+    oldLabel = $label_key
 } | ConvertTo-Json
