4a62b0874670a3138b209f478bd0bab48adc30b8
jpiccola <joey.piccola@gmail.com>
2020-11-10 22:51:23 -0700

    ts

--- site/jpi/plans/rt.pp
+++ site/jpi/plans/rt.pp
@@ -1,12 +1,12 @@
 plan jpi::rt (
   TargetSpec $nodes,
-  String $serial,
+  #String $serial,
   String $label_key,
   String $label_new,
 ) {
-    run_task('jpi::disk_set_status', $nodes, '_catch_errors' => true, status => 'online', serial => $serial)
+    #run_task('jpi::disk_set_status', $nodes, '_catch_errors' => true, status => 'online', serial => $serial)
     #run_task('jpi::disk_new_guid', $nodes, '_catch_errors' => true, serial => $serial)
     run_task('jpi::volume_set_label', $nodes, '_catch_errors' => true, label_key => $label_key, label_new => $label_new)
-    run_task('jpi::cluster_add_disk', $nodes, '_catch_errors' => true, name => $label_new, serial => $serial)
+    #run_task('jpi::cluster_add_disk', $nodes, '_catch_errors' => true, name => $label_new, serial => $serial)
     #run_task('jpi::cluster_add_csv', $nodes, '_catch_errors' => true, disk_resouce_name => $new_label)
 }
