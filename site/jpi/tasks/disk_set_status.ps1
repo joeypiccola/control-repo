b2321fa55fd210f725d69779ad173bffd389c157
jpiccola <joey.piccola@gmail.com>
2020-11-11 00:03:55 -0700

    make rw call its own thing

--- site/jpi/tasks/disk_set_status.ps1
+++ site/jpi/tasks/disk_set_status.ps1
@@ -4,10 +4,14 @@ Param(
     [string]$serial,
     [Parameter(Mandatory)]
     [ValidateSet('offline','online')]
-    [string]$status
+    [string]$status,
+    [Parameter()]
+    [boolean]$ensure_read_write = $true
 )
 
 $ErrorActionPreference = 'stop'
+
+#TODO maybe find way to write in hash existing and modified IsReadOnly details
 
 $disk = Get-Disk | Where-Object { $_.SerialNumber -eq $serial }
 
@@ -16,7 +20,11 @@ if (($disk | Measure-Object).count -eq 1) {
         'online' {
             # if the disk is not online bring it online!
             if ($disk.OperationalStatus -ne 'Online') {
-                Set-Disk -Number $disk.number -IsOffline $false -IsReadOnly $false
+                Set-Disk -Number $disk.number -IsOffline $false
+                # if the disk is read only and ensure r+w enabled then make it r+w
+                if ($disk.IsReadOnly -eq $true -and $ensure_read_write) {
+                    Set-Disk -Number $disk.number -IsReadOnly $false
+                }
             }
         }
         'offline' {
