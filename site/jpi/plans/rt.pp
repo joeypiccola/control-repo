f60cffaa0513c715d391d579e1feb3919a407a29
jpiccola <joey.piccola@gmail.com>
2020-11-10 23:53:49 -0700

    rm sleep fuckery

--- site/jpi/plans/rt.pp
+++ site/jpi/plans/rt.pp
@@ -5,7 +5,6 @@ plan jpi::rt (
   String $label_new,
 ) {
     run_task('jpi::disk_set_status', $nodes, '_catch_errors' => true, status => 'online', serial => $serial)
-    run_command('Start-Sleep -Seconds 20', $nodes)
     run_task('jpi::volume_set_label', $nodes, '_catch_errors' => true, label_key => $label_key, label_new => $label_new)
     run_task('jpi::cluster_add_disk', $nodes, '_catch_errors' => true, name => $label_new, serial => $serial)
     #run_task('jpi::cluster_add_csv', $nodes, '_catch_errors' => true, disk_resouce_name => $new_label)
