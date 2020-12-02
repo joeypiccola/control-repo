4e1875cef722928a2f276efb7c77d67757d7b5f5
jpiccola <joey.piccola@gmail.com>
2020-12-01 21:31:36 -0700

    add rm drive letter logic

--- site/jpi/tasks/disk_set_status.ps1
+++ site/jpi/tasks/disk_set_status.ps1
@@ -6,7 +6,9 @@ Param(
     [ValidateSet('offline','online')]
     [string]$status,
     [Parameter()]
-    [boolean]$ensure_read_write = $true
+    [boolean]$ensure_read_write = $true,
+    [Parameter()]
+    [boolean]$remove_drive_letter = $true
 )
 
 $ErrorActionPreference = 'stop'
@@ -18,12 +20,21 @@ $disk = Get-Disk | Where-Object { $_.SerialNumber -eq $serial }
 if (($disk | Measure-Object).count -eq 1) {
     switch ($status) {
         'online' {
-            # if the disk is not online bring it online!
+            # if the disk is not online optionally bring it online!
             if ($disk.OperationalStatus -ne 'Online') {
                 Set-Disk -Number $disk.number -IsOffline $false
                 # if the disk is read only and ensure r+w enabled then make it r+w
                 if ($disk.IsReadOnly -eq $true -and $ensure_read_write) {
                     Set-Disk -Number $disk.number -IsReadOnly $false
+                }
+            }
+            # if there is a drive letter optionally remove it
+            if ($remove_drive_letter -eq $true) {
+                $partitions = Get-Partition -DiskNumber $disk.Number
+                foreach ($partition in $partitions) {
+                    if ($partition.DriveLetter) {
+                        Remove-PartitionAccessPath -DiskNumber $disk.Number -PartitionNumber $partition.PartitionNumber -AccessPath "$($partition.DriveLetter):"
+                    }
                 }
             }
         }
