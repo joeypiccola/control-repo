f77e402d8e15353f47393281a4d7a29d6e9d60ef
jpiccola <joey.piccola@gmail.com>
2020-12-01 21:53:34 -0700

    make params opt and add reguid step

--- site/jpi/plans/rt.pp
+++ site/jpi/plans/rt.pp
@@ -1,14 +1,15 @@
 plan jpi::rt (
   TargetSpec $nodes,
-  String $serial,
-  String $label_key,
-  String $label_new,
+  Optional[String] $serial,
+  Optional[String] $label_key,
+  Optional[String] $label_new,
 ) {
     run_task('jpi::disk_set_status', $nodes, status => 'online', serial => $serial)
     run_task('jpi::volume_set_label', $nodes, label_key => $label_key, label_new => $label_new)
+    run_task('jpi::disk_new_guid', $nodes, serial => $serial)
     run_task('jpi::cluster_add_disk', $nodes, name => $label_new, serial => $serial)
     #run_task('jpi::cluster_add_csv', $nodes, '_catch_errors' => true, disk_resouce_name => $new_label)
-    #run_task('jpi::disk_new_guid', $nodes, '_catch_errors' => true, serial => $serial)
+
 }
 
 
