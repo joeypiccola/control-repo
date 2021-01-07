cba03a64277ad26945c76d2a4a47fcf30156f844
jpiccola <joey.piccola@gmail.com>
2021-01-07 12:59:51 -0700

    debug the wrong way

--- site/jpi/plans/ct_get_nodes.pp
+++ site/jpi/plans/ct_get_nodes.pp
@@ -2,6 +2,8 @@ plan jpi::ct_get_nodes (
   TargetSpec $nodes
 ) {
     $cluster_nodes = run_task('jpi::get_cluster_nodes', $nodes)
+    out::message($cluster_nodes)
     $clusternode_names = get_targets($cluster_nodes)
-    run_task('jpi::new_file', $clusternode_names)
+    out::message($clusternode_names)
+    #run_task('jpi::new_file', $clusternode_names)
 }
