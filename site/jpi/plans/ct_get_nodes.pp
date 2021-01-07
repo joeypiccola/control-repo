a3bb3c6da337d5ca8af1c7d6ba40baa488c76b28
jpiccola <joey.piccola@gmail.com>
2021-01-07 15:13:54 -0700

    ugh

--- site/jpi/plans/ct_get_nodes.pp
+++ site/jpi/plans/ct_get_nodes.pp
@@ -1,7 +1,8 @@
 plan jpi::ct_get_nodes (
   TargetSpec $nodes
 ) {
-    $cluster_nodes = run_task('jpi::get_cluster_nodes', $nodes)
-    $cluster_targets = get_targets($cluster_nodes['data'])
+    $cluster_nodes_results = run_task('jpi::get_cluster_nodes', $nodes)
+    $cluster_nodes = $cluster_nodes_results.results.map |$items| {$items.value['data']}
+    $cluster_targets = get_targets($cluster_nodes)
     run_task('jpi::new_file', $cluster_targets)
 }
