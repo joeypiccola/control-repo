b0bfb37ebdadca155cfab57d370fbc542bbaa851
jpiccola <joey.piccola@gmail.com>
2021-03-10 19:20:44 -0700

    scope to module

--- site/jpi/plans/plan_a.pp
+++ site/jpi/plans/plan_a.pp
@@ -1,7 +1,7 @@
 plan jpi::plan_a (
   TargetSpec $nodes,
 ) {
-  $some_value = lookup('junk::some_value')
+  $some_value = lookup('jpi::some_value')
 
   run_task('jpi::create_file', $nodes, file_contents => $some_value)
 }
