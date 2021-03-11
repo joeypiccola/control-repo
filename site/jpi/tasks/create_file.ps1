e9b7ad7d1981288943499dc520f2b7a03aea05fe
jpiccola <joey.piccola@gmail.com>
2021-03-10 19:08:01 -0700

    fix

--- site/jpi/tasks/create_file.ps1
+++ site/jpi/tasks/create_file.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 Param(
     [Parameter(Mandatory)]
-    [integer]$file_contents
+    [string]$file_contents
 )
 
 New-Item -ItemType File -Path 'C:\myFile.txt' -Value $file_contents
