24bb6da6b2cef2d8141c7a2004439c1bbff02b87
jpiccola <joey.piccola@gmail.com>
2021-03-11 21:52:55 -0700

    edit

--- site/jpi/plans/extend_disk.pp
+++ site/jpi/plans/extend_disk.pp
@@ -9,7 +9,7 @@ plan jpi::extend_disk (
   $powercli_proxy_host = lookup('jpi::powercli_proxy_host')
   # set single TargetSpec from the previously lookup'd powercli_proxy_host
   $powercli_proxy_host_target = get_target($powercli_proxy_host)
-  run_task('jpi::getvm', $powercli_proxy_host_target,
+  run_task('jpi::validate_target_node_in_vmware', $powercli_proxy_host_target,
     username    => $svc_vmware_username,
     password    => $svc_vmware_password,
     vcenter     => $vcenter,
--- site/jpi/tasks/validate_target_node_in_vmware.ps1
+++ site/jpi/tasks/validate_target_node_in_vmware.ps1
@@ -11,13 +11,26 @@ Param (
     [string]$target_node
 )
 
+# secure creds
 [securestring]$secStringPassword = ConvertTo-SecureString $password -AsPlainText -Force
 [pscredential]$credObject = New-Object System.Management.Automation.PSCredential ($username, $secStringPassword)
-
+# load mods
 $mods = ('VMware.VimAutomation.Cis.Core','VMware.VimAutomation.Common','VMware.VimAutomation.Core','VMware.VimAutomation.Sdk')
 Import-Module $mods | Out-Null
-
+# connect to vmware
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -ParticipateInCeip:$false -Scope AllUsers -Confirm:$false -DisplayDeprecationWarnings:$false | Out-Null
 Connect-VIServer -Server $vcenter -Credential $credObject | Out-Null
-Get-VM $target_node | Select-Object -Property name, powerstate, guest, vmhost, memorygb, numcpu, folder, resourcepool, version | ConvertTo-Json -Depth 1
+
+# validate the vm
+
+# split the passed name into hostname only
+$vm = Get-VM -Name $target_node.Split('.')[0] -ErrorAction Stop
+# if a vm was found ensure passed FQDN matches hostname known by VMware Tools
+if ($vm.Guest.HostName -eq $system) {
+    $vm | Select-Object -Property name, powerstate, guest, vmhost, memorygb, numcpu, folder, resourcepool, version | ConvertTo-Json -Depth 1
+} else {
+    Write-Error "VM found for $target_name does not match VMware guest hostname of $($vm.Guest.HostName)."
+}
+
+# disconnect from vmware
 Disconnect-VIServer -Confirm:$false -Force | Out-Null
