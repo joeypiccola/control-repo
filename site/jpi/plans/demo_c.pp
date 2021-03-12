5e2dc160e6e9523af99ce2770e22d35f9a83ca9e
jpiccola <joey.piccola@gmail.com>
2021-03-12 00:20:57 -0700

    fun

--- site/jpi/plans/demo_c.pp
+++ site/jpi/plans/demo_c.pp
@@ -2,6 +2,13 @@ plan jpi::demo_c (
   String $service
 ) {
   $target_node = 'jenkins.ad.piccola.us'
-  run_command('# forward email', $target_node)
-  run_command('# forward more email', $target_node)
+  run_command('<#   get_mailbox   #>', $target_node)
+  run_command('<#   parse_email   #>', $target_node)
+  run_command('<#   delete_spam   #>', $target_node)
+  run_command('<#   remove_all_training   #>', $target_node)
+  run_command('<#   analyze_email   #>', $target_node)
+  run_command('<#   analyze_more_email   #>', $target_node)
+  run_command('<#   analyze_even_more_email   #>', $target_node)
+  run_command('<#   so_much_email   #>', $target_node)
+  run_command('<#   forward_all_email   #>', $target_node)
 }
