26556ecc327af0032f0cde93a2df14a55819cabe
jpiccola <joey.piccola@gmail.com>
2021-03-12 00:17:04 -0700

    fix

--- site/jpi/plans/demo_c.pp
+++ site/jpi/plans/demo_c.pp
@@ -2,6 +2,6 @@ plan jpi::demo_c (
   String $service
 ) {
   $target_node = 'jenkins.ad.piccola.us'
-  run_command('# forward email', $nodes)
-  run_command('# forward more email', $nodes)
+  run_command('# forward email', $target_node)
+  run_command('# forward more email', $target_node)
 }
