01e08c75dcc1f0cb846bb08db81fd2867e2eab85
jpiccola <joey.piccola@gmail.com>
2021-06-02 23:00:00 -0600

    fix: scope task correctly

--- site/jpi/plans/reboot.pp
+++ site/jpi/plans/reboot.pp
@@ -1,7 +1,7 @@
 plan jpi::reboot (
   TargetSpec $nodes,
 ) {
-  run_task('create_file', $nodes,
+  run_task('jpi::create_file', $nodes,
     file_name => 'pre_reboot'
   )
   run_plan('reboot', $nodes,
@@ -10,7 +10,7 @@ plan jpi::reboot (
     retry_interval    => 10,  # check every 10s to see if system is back up
     disconnect_wait   => 120,  # how long to wait after reboot to start checking if system is back up
   )
-  run_task('create_file', $nodes,
+  run_task('jpi::create_file', $nodes,
     file_name => 'post_reboot'
   )
 }
