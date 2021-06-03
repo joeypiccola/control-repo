b941e358f2bb7ac8b666ea39c864d27d288c008c
jpiccola <joey.piccola@gmail.com>
2021-06-02 22:57:03 -0600

    fix: add misc task calls to see how things look in GUI

--- site/jpi/plans/reboot.pp
+++ site/jpi/plans/reboot.pp
@@ -1,10 +1,16 @@
 plan jpi::reboot (
   TargetSpec $nodes,
 ) {
+  run_task('create_file', $nodes,
+    file_name => 'pre_reboot'
+  )
   run_plan('reboot', $nodes,
     message           => 'Post DC promotion reboot. Triggered via Puppet.',
-    reconnect_timeout => 600, # check for a total of 600s if system is back up
+    reconnect_timeout => 600, # check for a total of 600s if system is back up, then give up
     retry_interval    => 10,  # check every 10s to see if system is back up
-    disconnect_wait   => 20,  # how long to wait after reboot to start checking if system is back up
+    disconnect_wait   => 120,  # how long to wait after reboot to start checking if system is back up
+  )
+  run_task('create_file', $nodes,
+    file_name => 'post_reboot'
   )
 }
